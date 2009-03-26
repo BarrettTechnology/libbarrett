3d824d2e2d6126ea93d6e37e09bff708f87af606
cd <cd@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2009-03-26 19:21:17 +0000

    The library now supports building only the asynchronous components ...

--- trunk/programs/examples/jsonpygtk/jsonpygtk.py
+++ trunk/programs/examples/jsonpygtk/jsonpygtk.py
@@ -85,10 +85,15 @@ class JsonClient:
       self.s.connect(('localhost',1338))
       
       # Attempt to open wam15
-      q = '{"method":"bt_wam_create","params":["wam4"]}\n'
+      q = '{"method":"bt_wam_create_opt","params":["wam4",0]}\n'
       print 'sending:', q
       self.s.send(q)
-      print 'received:', self.s.recv(100)
+      rec = self.s.recv(100)
+      tup = rec.split(':',1)
+      tup2 = tup[1].split('}',1)
+      self.wam = int(tup2[0])
+      print 'received:', rec
+      print 'wam number:', self.wam
    
    def go(self, q):
       print 'sending:', q
@@ -99,33 +104,33 @@ class JsonClient:
       return False
    
    def destroy(self, widget, data=None):
-      self.go('{"method":"bt_wam_destroy","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_destroy","params":[%d]}\n' % (self.wam,))
       self.s.close()
       gtk.main_quit()
       
    def gcomp_on(self, widget, data=None):
-      self.go('{"method":"bt_wam_setgcomp","params":["wam4",1]}\n')
+      self.go('{"method":"bt_wam_setgcomp","params":[%d,1]}\n' % (self.wam,))
    
    def gcomp_off(self, widget, data=None):
-      self.go('{"method":"bt_wam_setgcomp","params":["wam4",0]}\n')
+      self.go('{"method":"bt_wam_setgcomp","params":[%d,0]}\n' % (self.wam,))
    
    def do_idle(self, widget, data=None):
-      self.go('{"method":"bt_wam_idle","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_idle","params":[%d]}\n' % (self.wam,))
    
    def do_hold(self, widget, data=None):
-      self.go('{"method":"bt_wam_hold","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_hold","params":[%d]}\n' % (self.wam,))
    
    def teach_start(self, widget, data=None):
-      self.go('{"method":"bt_wam_teach_start","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_teach_start","params":[%d]}\n' % (self.wam,))
    
    def teach_end(self, widget, data=None):
-      self.go('{"method":"bt_wam_teach_end","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_teach_end","params":[%d]}\n' % (self.wam,))
    
    def teach_playback(self, widget, data=None):
-      self.go('{"method":"bt_wam_playback","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_playback","params":[%d]}\n' % (self.wam,))
    
    def move_home(self, widget, data=None):
-      self.go('{"method":"bt_wam_movehome","params":["wam4"]}\n')
+      self.go('{"method":"bt_wam_movehome","params":[%d]}\n' % (self.wam,))
       
    def quit_button(self, widget, data=None):
       if not self.quit_request(self.window,None):
