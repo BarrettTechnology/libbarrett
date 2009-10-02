527d9effc843c89ce0b9bc3d5af6d37ae491d6f4
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2009-10-02 17:28:53 +0000

    First stab at scripts to outsource build to a WAM.

--- trunk/common/prebuild.sh
+++ trunk/common/prebuild.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# This script is designed to be run by Eclipse from a project's build directory
+
 scriptpath=`dirname $0`
 
-$scriptpath/cpplint.py --filter=-build/header_guard,-build/include_order,-legal,-readability/streams,-whitespace/blank_line,-whitespace/braces,-whitespace/tab,-whitespace/labels  --recursive ../src/
+# run cpplint on the project
+if [ "$1" != "--no-cpplint" ]
+then
+	$scriptpath/cpplint.py \
+		--filter=-build/header_guard,-build/include_order,-legal,-readability/streams,-whitespace/blank_line,-whitespace/braces,-whitespace/tab,-whitespace/labels \
+		--recursive ../src/
+else
+	echo "Skipping cpplint."
+fi
 
