aa805437dc45b629ef6896331506ce35958e6ba3
Dan Cody <dtydc5@users.noreply.github.com>
2009-10-30 03:44:54 +0000

    Removed AbstractController and AbstractController's virtual methods from all decendents. The class was only used by the old SupervisoryController.

--- library-tests/src/systems/abstract/controller.cpp
+++ library-tests/src/systems/abstract/controller.cpp
@@ -17,17 +17,6 @@ namespace {
 using namespace barrett;
 
 
-// we just want this to compile
-TEST(AbstractControllerTest, Interface) {
-	ControllerImpl<double> c;
-	systems::AbstractController& ac = c;
-
-	ac.getReferenceInput();
-	ac.getFeedbackInput();
-	ac.getControlOutput();
-}
-
-
 // TODO(dc): actually test this
 class ControllerTest : public ::testing::Test {
 public:
@@ -50,25 +39,6 @@ TEST_F(ControllerTest, InitialOutputValueCtor) {
 	EXPECT_TRUE(eios.inputValueDefined()) << "initial value undefined";
 	EXPECT_EQ(-88.1, eios.getInputValue())
 		<< "wrong initial value given";
-}
-
-TEST_F(ControllerTest, GetReferenceInput) {
-	EXPECT_EQ(&controller.referenceInput, controller.getReferenceInput())
-		<< "getReferenceInput() didn't return &controller.referenceInput";
-}
-
-TEST_F(ControllerTest, GetFeedbackInput) {
-	EXPECT_EQ(&controller.feedbackInput, controller.getFeedbackInput())
-		<< "getFeedbackInput() didn't return &controller.feedbackInput";
-}
-
-TEST_F(ControllerTest, GetControlOutput) {
-	EXPECT_EQ(&controller.controlOutput, controller.getControlOutput())
-		<< "getControlOutput() didn't return &controller.controlOutput";
-}
-
-TEST_F(ControllerTest, SelectAdapter) {
-	// TODO(dc): stub
 }
 
 
