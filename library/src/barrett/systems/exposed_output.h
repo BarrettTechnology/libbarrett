4b1c01b02fc6b30aa8ec1bc00adcf66e5d896871
Dan Cody <dtydc5@users.noreply.github.com>
2009-11-13 18:04:35 +0000

    Intermediary commit.

---
+++ library/src/barrett/systems/exposed_output.h
@@ -0,0 +1,50 @@
+/*
+ * exposed_output.h
+ *
+ *  Created on: Nov 13, 2009
+ *      Author: dc
+ */
+
+#ifndef EXPOSED_OUTPUT_H_
+#define EXPOSED_OUTPUT_H_
+
+
+#include "../detail/ca_macro.h"
+#include "./abstract/system.h"
+
+
+namespace barrett {
+namespace systems {
+
+
+template <typename T>
+class ExposedOutput {
+// IO
+public:		System::Output<OutputType> output;
+protected:	typename System::Output<OutputType>::Value* outputValue;
+
+
+public:
+	ExposedOutput() :
+		output(&outputValue) {}
+	explicit ExposedOutput(const T& initialValue) :
+		output(initialValue, &outputValue) {}
+
+	void setValue(const T& value) {
+		outputValue->setValue(value);
+	}
+	void setValueUndefined() {
+		outputValue->setValueUndefined();
+	}
+	void delegateTo(const System::Output<T>& delegate) {
+		outputValue->delegateTo(delegate);
+	}
+
+private:
+	DISALLOW_COPY_AND_ASSIGN(ExposedOutput);
+};
+
+
+}
+}
+#endif /* EXPOSED_OUTPUT_H_ */
