88d6ee06123370d95d3c147296a241fd223a014d
Dan Cody <dtydc5@users.noreply.github.com>
2010-01-13 18:50:29 +0000

    Minor changes.

--- library/src/barrett/systems/periodic_data_logger.h
+++ library/src/barrett/systems/periodic_data_logger.h
@@ -10,7 +10,6 @@
 
 
 #include "../detail/ca_macro.h"
-#include "../thread/abstract/mutex.h"
 #include "./abstract/system.h"
 #include "../log/real_time_writer.h"
 
@@ -26,44 +25,15 @@ public:	Input<T> input;
 
 
 public:
-	PeriodicDataLogger(LogWriterType* logWriter, size_t periodMultiplier = 10) :
-		System(true), input(this),
-		lw(logWriter), logging(true),
-		ecCount(0), ecCountRollover(periodMultiplier) {}
+	PeriodicDataLogger(LogWriterType* logWriter, size_t periodMultiplier = 10);
 
-	virtual ~PeriodicDataLogger() {
-		if (isLogging()) {
-			closeLog();
-		}
-	}
-
-	bool isLogging() {
-		return logging;
-	}
-
-	void closeLog() {
-		if (isLogging()) {
-			thread::Mutex& emMutex = getEmMutex();
-			emMutex.lock();
-			logging = false;
-			emMutex.unlock();
-
-			// lw::close() is probably not real-time safe, so keep it out of the critical section.
-			lw->close();
-			delete lw;
-			lw = NULL;
-		}
-	}
+	virtual ~PeriodicDataLogger();
+	bool isLogging();
+	void closeLog();
 
 protected:
-	virtual bool inputsValid() {
-		ecCount = (ecCount + 1) % ecCountRollover;
-		return logging  &&  ecCount == 0  &&  input.valueDefined();
-	}
-
-	virtual void operate() {
-		lw->putRecord(input.getValue());
-	}
+	virtual bool inputsValid();
+	virtual void operate();
 
 	// Optimization: this System has no Outputs to invalidate.
 	virtual void invalidateOutputs() {}
@@ -79,6 +49,10 @@ private:
 
 }
 }
+
+
+// include template definitions
+#include "./detail/periodic_data_logger-inl.h"
 
 
 #endif /* PERIODIC_DATA_LOGGER_H_ */
