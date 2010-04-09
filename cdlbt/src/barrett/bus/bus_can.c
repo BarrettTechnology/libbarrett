bfa118fe1189aa2f23daf1eadb7d4c4177bde535
Dan Cody <dtydc5@users.noreply.github.com>
2010-04-09 18:14:13 +0000

    Reverted bus_can.c changes from r258. These were debugging changes that need further work.

--- cdlbt/src/barrett/bus/bus_can.c
+++ cdlbt/src/barrett/bus/bus_can.c
@@ -376,13 +376,8 @@ int bt_bus_can_clearmsg(struct bt_bus_can * dev)
    /*find a better way of clearing the bus*/
    while (read_msg(dev, &id, &len, data, 0) == 0)
    {
-	   int in_property, ispacked;
-	   long reply;
-	   parse_msg(id, len, data, &id, &in_property, &ispacked, &reply);
-	   if (id < 11  || id > 14) {
-		   syslog(LOG_ERR, "Cleared unexpected message CANbus from ID:%d", id);
-		      bt_os_usleep(1);
-	   }
+      syslog(LOG_ERR, "Cleared unexpected message from CANbus.");
+      bt_os_usleep(1);
    }
    return 0;
    
@@ -568,10 +563,8 @@ int bt_bus_can_get_packed(struct bt_bus_can * dev, int group, int how_many,
       err = parse_msg(msgID, len, packet, &id, &in_property, &ispacked, &reply);
       if(ispacked)
       {
-    	  if (id < 11  ||  id > 14) {
-			 data[id] = reply;
-			 how_many--;
-    	  }
+         data[id] = reply;
+         how_many--;
       }
       else
       {
