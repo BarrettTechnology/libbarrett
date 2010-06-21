1b436ec12e75d2fdfa163d627eeb834e86e64e8a
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-06-21 21:44:29 +0000

    Removed config file dependence from can_terminal.cpp and log_ft_data.cpp.

--- trunk/sandbox/src/can_terminal.cpp
+++ trunk/sandbox/src/can_terminal.cpp
@@ -26,17 +26,30 @@ void waitForEnter() {
 }
 
 
-int main() {
-	libconfig::Config config;
-	config.readFile("/etc/wam/wam7-new.config");
-
+int main(int argc, char** argv) {
 	mlockall(MCL_CURRENT|MCL_FUTURE);
 	rt_task_shadow(new RT_TASK, NULL, 10, 0);
 
-	struct bt_bus* bus;
-	if (bt_bus_create(&bus, config.lookup("wam.low_level.bus").getCSetting(), bt_bus_UPDATE_POS_DIFF)) {
-		printf("Couldn't create bus.\n");
-		return 1;
+
+	int port = 0;
+	switch (argc) {
+	case 1:
+		printf("No port argument given. Using default.\n");
+		break;
+	case 2:
+		port = atoi(argv[1]);
+		break;
+	default:
+		printf("ERROR: Expected 1 or 0 arguments.\n");
+		return -1;
+		break;
+	}
+
+	printf("Using CAN bus port %d.\n", port);
+	bt_bus_can* dev = NULL;
+	if (bt_bus_can_create(&dev, port)) {
+		printf("Couldn't open the CAN bus.\n");
+		return -1;
 	}
 
 
@@ -58,14 +71,14 @@ int main() {
 			cin >> property;
 			cout << "\tValue: ";
 			cin >> value;
-			bt_bus_set_property(bus, id, property, value);
+			bt_bus_can_set_property(dev, id, property, value);
 			printf("SET %d: %d = %ld\n", id, property, value);
 			break;
 
 		case 'g':
 			cout << "\tProperty: ";
 			cin >> property;
-			bt_bus_can_get_property(bus->dev, id, property, &value, NULL, 1);
+			bt_bus_can_get_property(dev, id, property, &value, NULL, 1);
 			printf("GET %d: %d = %ld\n", id, property, value);
 
 		default:
