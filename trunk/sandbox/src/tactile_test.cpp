75a806aac1b500f9325956866f97905bb91109b5
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-09-07 22:12:24 +0000

    Modified tactile_test.cpp to support turning on hand motors. This helps spot EMI issues with the TACT sensors. Also (potentially) resolved a bug when using the PCAN driver that can cause a full system lockup.

--- trunk/sandbox/src/tactile_test.cpp
+++ trunk/sandbox/src/tactile_test.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <vector>
 #include <string>
 #include <cstdlib>
 
@@ -24,9 +25,75 @@ const int HAND_DOF = 4;
 #define HEIGHT 3
 
 
+enum btkey {
+   BTKEY_UNKNOWN = -2,
+   BTKEY_NOKEY = -1,
+   BTKEY_TAB = 9,
+   BTKEY_ENTER = 10,
+   BTKEY_ESCAPE = 27,
+   BTKEY_BACKSPACE = 127,
+   BTKEY_UP = 256,
+   BTKEY_DOWN = 257,
+   BTKEY_LEFT = 258,
+   BTKEY_RIGHT = 259
+};
+enum btkey btkey_get()
+{
+   int c1,c2,c3;
+
+   /* Get the key from ncurses */
+   c1 = getch();
+   if (c1 == ERR) return BTKEY_NOKEY;
+
+   /* Get all keyboard characters */
+   if (32 <= c1 && c1 <= 126) return (enum btkey)c1;
+
+   /* Get special keys */
+   switch (c1)
+   {
+      case BTKEY_TAB:
+      case BTKEY_ENTER:
+      case BTKEY_BACKSPACE:
+            return (enum btkey)c1;
+      /* Get extended keyboard chars (eg arrow keys) */
+      case 27:
+         c2 = getch();
+         if (c2 == ERR) return BTKEY_ESCAPE;
+         if (c2 != 91) return BTKEY_UNKNOWN;
+         c3 = getch();
+         switch (c3)
+         {
+            case 65: return BTKEY_UP;
+            case 66: return BTKEY_DOWN;
+            case 67: return BTKEY_RIGHT;
+            case 68: return BTKEY_LEFT;
+            default: return BTKEY_UNKNOWN;
+         }
+      default:
+         return BTKEY_UNKNOWN;
+   }
+}
+
 void waitForEnter() {
 	static std::string line;
 	std::getline(std::cin, line);
+}
+
+void drawMenu(WINDOW *win, int starty, int startx, const std::string& title, const std::vector<std::string>& labels, const std::vector<bool>& checked, size_t activeIndex) {
+	mvwprintw(win, starty,startx, title.c_str());
+
+	for (size_t i = 0; i < labels.size(); ++i) {
+		if (i == activeIndex) {
+			wstandout(win);
+		}
+		mvwprintw(win, starty+i+1,startx, "   %s", labels[i].c_str());
+		if (checked[i]) {
+			mvwaddch(win, starty+i+1,startx+1, 'X' | A_BOLD);
+		}
+		if (i == activeIndex) {
+			wstandend(win);
+		}
+	}
 }
 
 void board(WINDOW *win, int starty, int startx, int lines, int cols, int tile_width, int tile_height)
@@ -144,7 +211,7 @@ int main(int argc, char** argv) {
 	printf("Waking hand pucks ...\n");
 
 	for(int i = 11; i <= 14; i++) {
-		bt_bus_can_set_property(dev, i, 5, 2); // Set STAT to STATUS_READY
+		bt_bus_can_set_property(dev, i, 5, 2);  // Set STAT to STATUS_READY
 	}
 	usleep((long)1e6);
 
@@ -158,7 +225,7 @@ int main(int argc, char** argv) {
 			printf("Found Puck %d.\n", i);
 
 			if (value1 == 0) {
-				bt_bus_can_set_property(dev, i, 5, 2); // Set STAT to STATUS_READY
+				bt_bus_can_set_property(dev, i, 5, 2);  // Set STAT to STATUS_READY
 				usleep((long)1e6);
 			}
 
@@ -187,17 +254,49 @@ int main(int argc, char** argv) {
 	std::cin >> streamId;
 	waitForEnter();
 
+
 	initscr();
 	curs_set(0);
 	noecho();
 	timeout(0);
     board(stdscr, 0,0, 8,3, WIDTH,HEIGHT);
 
+    const std::string title("Active motors:");
+    const size_t numMenuItems = 5;
+    std::vector<std::string> labels;
+    labels.push_back("Puck 11");
+    labels.push_back("Puck 12");
+    labels.push_back("Puck 13");
+    labels.push_back("Puck 14");
+    labels.push_back("Exit");
+
+    std::vector<bool> checked(numMenuItems, false);
+    size_t activeIndex = 0;
+
+    drawMenu(stdscr, 5,20, title, labels, checked, activeIndex);
+
 	while (true) {
-		// exit on key press
-		usleep(100000);
-		if (getch() != ERR) {
-			break;
+		btkey c = btkey_get();
+		if (c != BTKEY_NOKEY) {
+			if (c == BTKEY_UP  &&  activeIndex != 0) {
+				--activeIndex;
+			} else if (c == BTKEY_DOWN  &&  activeIndex != (numMenuItems - 1)) {
+				++activeIndex;
+			} else if (c == BTKEY_ENTER) {
+				if (activeIndex == (numMenuItems - 1)) {
+					break;
+				} else {
+					checked[activeIndex] = !checked[activeIndex];
+					if (checked[activeIndex]) {
+						bt_bus_can_set_property(dev, activeIndex+11, 78, 0);  // Set TSTOP to 0
+						bt_bus_can_set_property(dev, activeIndex+11, 8, 2);  // Set MODE to TORQUE
+					} else {
+						bt_bus_can_set_property(dev, activeIndex+11, 8, 0);  // Set MODE to IDLE
+					}
+				}
+			}
+
+		    drawMenu(stdscr, 5,20, title, labels, checked, activeIndex);
 		}
 
 //		// wait for key press
@@ -206,14 +305,21 @@ int main(int argc, char** argv) {
 //		}
 
 		bt_bus_can_set_property(dev, streamId, 106, 2);
+		usleep(100000);
 		osTact = 5;  // 5 messages are returned for full TACT data
 		while (osTact--) {
-			bt_bus_can_async_read(dev, &id, NULL, &value1, NULL, data, 1, 1);
+			bt_bus_can_async_read(dev, &id, NULL, &value1, NULL, data, 0, 1);
 		    graphMessage(stdscr, 0,0, data);
 		}
 	}
 
 	endwin();
+
+	for (int i = 0; i < HAND_DOF; ++i) {
+		if (checked[i]) {
+			bt_bus_can_set_property(dev, i+11, 8, 0);  // Set MODE to IDLE
+		}
+	}
 
 	return 0;
 }
