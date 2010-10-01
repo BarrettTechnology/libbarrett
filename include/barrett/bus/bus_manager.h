b1bfce2795998c3bf4251a6ddc75562838d35dd8
Dan Cody <dtydc5@users.noreply.github.com>
2010-10-01 21:19:14 +0000

    Minor changes.

--- include/barrett/bus/bus_manager.h
+++ include/barrett/bus/bus_manager.h
@@ -38,9 +38,13 @@ public:
 	virtual void close() { bus.close(); }
 	virtual bool isOpen() { return bus.isOpen(); }
 
-	virtual int send(int busId, const unsigned char* data, size_t len) const { return bus.send(busId, data, len); }
-	virtual int receive(int expectedBusId, unsigned char* data, size_t& len, bool blocking = true, bool realtime = false) const;
-	virtual int receiveRaw(int& busId, unsigned char* data, size_t& len, bool blocking = true) const { return bus.receiveRaw(busId, data, len, blocking); }
+	virtual int send(int busId, const unsigned char* data, size_t len) const
+		{ return bus.send(busId, data, len); }
+	virtual int receive(int expectedBusId, unsigned char* data, size_t& len,
+			bool blocking = true, bool realtime = false) const;
+	virtual int receiveRaw(int& busId, unsigned char* data, size_t& len,
+			bool blocking = true) const
+		{ return bus.receiveRaw(busId, data, len, blocking); }
 
 
 protected:
