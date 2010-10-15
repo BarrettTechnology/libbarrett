9d170832f6b4d26da44cccc2da863ad02e8546d3
Dan Cody <dtydc5@users.noreply.github.com>
2010-10-15 17:11:30 +0000

    The user can now "break into" and "brak out of" the haptic ball and box. Code needs to be a little more configurable/flexible (set threshold depth, turn off break-through, etc.).

--- include/barrett/systems/haptic_ball.h
+++ include/barrett/systems/haptic_ball.h
@@ -13,6 +13,7 @@
 
 #include <barrett/detail/ca_macro.h>
 #include <barrett/units.h>
+#include <barrett/math.h>
 #include <barrett/systems/abstract/haptic_object.h>
 
 
@@ -23,29 +24,36 @@ namespace systems {
 class HapticBall : public HapticObject {
 public:
 	HapticBall(units::CartesianPosition::type center, double radius) :
-		c(center), r(radius) {}
+		c(center), r(radius), keepOutside(true) /* doesn't matter how this is initialized, it'll fix itself */ {}
 	virtual ~HapticBall() {}
 
 protected:
 	virtual void operate() {
 		units::CartesianForce::type error = input.getValue() - c;
 		double mag = error.norm();
-		//double mag = std::sqrt(error[0]*error[0] + error[1]*error[1] + error[2]*error[2]);
 
-		if (mag < r) {  // if we are inside the ball
-			depthOutputValue->setValue(r-mag);
+		bool outside = mag > r;
 
-			// unit vector pointing towards the surface of the ball
-			directionOutputValue->setValue(error/mag);
-		} else {
-			depthOutputValue->setValue(0.0);
-//			directionOutputValue->setValue(units::CartesianForce::type::Zero());
-			directionOutputValue->setValue(units::CartesianForce::type(0.0));
+		// if we are inside the ball and we shouldn't be, or we are outside the ball and we shouldn't be
+		if ((keepOutside && !outside)  || (!keepOutside && outside)) {
+			if (math::abs(r-mag) > 0.02) {
+				keepOutside = !keepOutside;
+			} else {
+				depthOutputValue->setValue(r-mag);
+
+				// unit vector pointing towards the surface of the ball
+				directionOutputValue->setValue(error/mag);
+				return;
+			}
 		}
+
+		depthOutputValue->setValue(0.0);
+		directionOutputValue->setValue(units::CartesianForce::type(0.0));
 	}
 
 	units::CartesianPosition::type c;
 	double r;
+	bool keepOutside;
 
 private:
 	DISALLOW_COPY_AND_ASSIGN(HapticBall);
--- include/barrett/systems/haptic_box.h
+++ include/barrett/systems/haptic_box.h
@@ -13,6 +13,7 @@
 
 #include <barrett/detail/ca_macro.h>
 #include <barrett/units.h>
+#include <barrett/math.h>
 #include <barrett/systems/abstract/haptic_object.h>
 
 
@@ -23,39 +24,63 @@ namespace systems {
 class HapticBox : public HapticObject {
 public:
 	HapticBox(units::CartesianPosition::type center, double xSize, double ySize, double zSize) :
-//		c(center), x(xSize/2), y(ySize/2), z(zSize/2), pos() {}
-		c(center), size(xSize/2, ySize/2, zSize/2), inBox(false), index(-1) {}
+		c(center), size(xSize/2, ySize/2, zSize/2), inBox(false), index(-1), keepOutside(true) {}
 	virtual ~HapticBox() {}
 
 protected:
 	virtual void operate() {
 		pos = input.getValue() - c;
 
-		if ((pos.cwise().abs().cwise() < size).all()) {  // if we are inside the box
+		bool outside = (pos.cwise().abs().cwise() > size).any();
+
+		// if we are inside the box and we shouldn't be
+		if (keepOutside  &&  !outside) {
 			if ( !inBox ) {  // if we weren't in the box last time
 				// find out what side we entered on
 				(size - pos.cwise().abs()).minCoeff(&index);
 			}
 
-			depthOutputValue->setValue(size[index] - math::abs(pos[index]));
-			directionOutputValue->setValue(math::sign(pos[index]) * units::CartesianForce::type::Unit(index));
+			double depth = size[index] - math::abs(pos[index]);
+			if (depth > 0.02) {
+				keepOutside = !keepOutside;
+			} else {
+				depthOutputValue->setValue(depth);
+				directionOutputValue->setValue(math::sign(pos[index]) * units::CartesianForce::type::Unit(index));
 
-			inBox = true;
-		} else {
-			inBox = false;
-			depthOutputValue->setValue(0.0);
-			directionOutputValue->setValue(units::CartesianForce::type(0.0));
+				inBox = true;
+				return;
+			}
 		}
+		inBox = false;
+
+		// if we are outside the box and we shouldn't be
+		if (!keepOutside  &&  outside) {
+			units::CartesianForce::type dir = size - pos.cwise().abs();
+			dir = dir.cwise() * (dir.cwise() < 0.0).cast<double>();
+			dir = dir.cwise() * math::sign(pos);
+
+			double depth = dir.norm();
+			if (depth > 0.02) {
+				keepOutside = !keepOutside;
+			} else {
+				depthOutputValue->setValue(depth);
+				directionOutputValue->setValue(dir/depth);
+				return;
+			}
+		}
+
+		depthOutputValue->setValue(0.0);
+		directionOutputValue->setValue(units::CartesianForce::type(0.0));
 	}
 
 	units::CartesianPosition::type c;
-//	double x,y,z;
 	math::Vector<3>::type size;
 
-	// temporaries
+	// state & temporaries
 	units::CartesianForce::type pos;
 	bool inBox;
 	int index;
+	bool keepOutside;
 
 private:
 	DISALLOW_COPY_AND_ASSIGN(HapticBox);
