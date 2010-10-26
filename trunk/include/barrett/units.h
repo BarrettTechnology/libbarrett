2b0d8bc65081a974768b948d8afd5f5811cf63ac
wp <wp@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-10-26 16:01:34 +0000

    changed two documentation thigns

--- trunk/include/barrett/units.h
+++ trunk/include/barrett/units.h
@@ -39,8 +39,8 @@
  * particular WAM) 7-element arrays of \c doubles, they represent two different quantities. They are not interchangeable. They have different %units. A
  * user might want to design a joint-space position
  * controller and a joint-space velocity controller for the WAM. If the joint position output of the barrett::Wam were to be connected to the velocity
- * controller's feedback input, it would almost certainly be a programmer error. If such a program were run, the robot would not behave as intended. The
- * programmer might spend a long time chasing down the bug.
+ * controller's feedback input, it would almost certainly be a programmer error. If such a program were run, the robot would not behave as intendend and
+ * finding the bug could be difficult and time-consuming.
  *
  * Adding a notion of %units to our vectors (units::JointPositions::type, units::JointVelocities::type, etc.) allows us to:
  *   - be explicit about our intents for any given input or parameter
@@ -65,7 +65,7 @@
 
 /** Creates a standard set of \c typedefs in the local scope for all built-in barrett::units.
  *
- * Many classes use this macro to define internal short-hand names for the barrett::units they make frequent use of.
+ * Many classes use this macro to define internal short-hand names for the barrett::units they use frequently.
  *
  * \param dimension This positive integer is used in the \c typedefs below as the template parameter for those types that require one.
  *
