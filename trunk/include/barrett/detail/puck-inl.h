4369cd4e28dbce80f87afff5b36a8dabeaeacb03
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-11-01 16:23:57 +0000

    Refactored Puck::getProperty() and friends to support a templated message parsing mechanism. The only Parser provided is Puck::StandardParser; others will be written to accommodate non-standard message formats. Non-templated wrappers are provided that use the StandardParser.

--- trunk/include/barrett/detail/puck-inl.h
+++ trunk/include/barrett/detail/puck-inl.h
@@ -5,8 +5,27 @@
  *      Author: dc
  */
 
+#include <barrett/puck_group.h>
+
+
 namespace barrett {
 
+
+inline int Puck::getProperty(enum Property prop) const {
+	return getProperty(bus, id, getPropertyId(prop));
+}
+inline int Puck::tryGetProperty(enum Property prop, bool* successful, int timeout_us) const {
+	return tryGetProperty(bus, id, getPropertyId(prop), successful, timeout_us);
+}
+
+template<typename Parser>
+inline typename Parser::result_type Puck::getProperty(enum Property prop) const {
+	return getProperty<Parser> (bus, id, getPropertyId(prop));
+}
+template<typename Parser>
+inline typename Parser::result_type Puck::tryGetProperty(enum Property prop, bool* successful, int timeout_us) const {
+	return tryGetProperty<Parser> (bus, id, getPropertyId(prop), successful, timeout_us);
+}
 
 template<template<typename U, typename = std::allocator<U> > class Container>
 void Puck::wake(Container<Puck*> pucks)
@@ -73,6 +92,104 @@ void Puck::wake(Container<Puck*> pucks)
 	}
 }
 
+inline int Puck::getProperty(const CommunicationsBus& bus, int id, int propId)
+{
+	return getProperty<StandardParser>(bus, id, propId);
+}
+// TODO(dc): throw exception if getProperty is called with a group ID?
+template<typename Parser>
+typename Parser::result_type Puck::getProperty(const CommunicationsBus& bus, int id, int propId)
+{
+	int retCode;
+	typename Parser::result_type value = getPropertyHelper<Parser>(bus, id, propId, true, &retCode, 0);
+	if (retCode != 0) {
+		syslog(LOG_ERR, "%s: Puck::receiveGetPropertyReply() returned error %d.", __func__, retCode);
+		throw std::runtime_error("Puck::getProperty(): Failed to receive reply. Check /var/log/syslog for details.");
+	}
+	return value;
+}
+
+inline int Puck::tryGetProperty(const CommunicationsBus& bus, int id, int propId, bool* successful, int timeout_us)
+{
+	return tryGetProperty<StandardParser>(bus, id, propId, successful, timeout_us);
+}
+template<typename Parser>
+typename Parser::result_type Puck::tryGetProperty(const CommunicationsBus& bus, int id, int propId, bool* successful, int timeout_us)
+{
+	int retCode;
+	typename Parser::result_type value = getPropertyHelper<Parser>(bus, id, propId, false, &retCode, timeout_us);
+	if (retCode == 0) {
+		*successful = true;
+	} else {
+		*successful = false;
+		if (retCode != 1) {  // some error other than "would block" occurred
+			syslog(LOG_ERR, "%s: Puck::receiveGetPropertyReply() returned error %d.", __func__, retCode);
+			throw std::runtime_error("Puck::tryGetProperty(): Receive error. Check /var/log/syslog for details.");
+		}
+	}
+
+	return value;
+}
+
+template<typename Parser>
+typename Parser::result_type Puck::getPropertyHelper(const CommunicationsBus& bus, int id, int propId, bool blocking, int* retCode, int timeout_us)
+{
+	*retCode = sendGetPropertyRequest(bus, id, propId);
+	if (*retCode != 0) {
+		syslog(LOG_ERR, "%s: Puck::sendGetPropertyRequest() returned error %d.", __func__, *retCode);
+		throw std::runtime_error("Puck::getPropertyHelper(): Failed to send request. Check /var/log/syslog for details.");
+	}
+
+	if (timeout_us != 0) {
+		usleep(timeout_us);
+	}
+
+	return receiveGetPropertyReply<Parser>(bus, id, propId, blocking, retCode);
+}
+
+inline void Puck::setProperty(const CommunicationsBus& bus, int id, int propId, int value)
+{
+	static const size_t MSG_LEN = 6;
+
+	unsigned char data[MSG_LEN];
+	data[0] = (propId & PROPERTY_MASK) | SET_MASK;
+	data[1] = 0;
+	data[2] = (value & 0x000000ff);
+	data[3] = (value & 0x0000ff00) >>  8;
+	data[4] = (value & 0x00ff0000) >> 16;
+	data[5] = (value & 0xff000000) >> 24;
+
+	int ret = bus.send(nodeId2BusId(id), data, MSG_LEN);
+	if (ret != 0) {
+		syslog(LOG_ERR, "%s: Puck::setProperty() returned error %d.", __func__, ret);
+		throw std::runtime_error("Puck::setProperty(): Failed to send SET message. Check /var/log/syslog for details.");
+	}
+}
+
+inline int Puck::sendGetPropertyRequest(const CommunicationsBus& bus, int id, int propId)
+{
+	static const size_t MSG_LEN = 1;
+
+	unsigned char data[MSG_LEN];
+	data[0] = propId & PROPERTY_MASK;
+
+	return bus.send(nodeId2BusId(id), data, MSG_LEN);
+}
+
+template<typename Parser>
+typename Parser::result_type Puck::receiveGetPropertyReply(const CommunicationsBus& bus, int id, int propId, bool blocking, int* retCode)
+{
+	unsigned char data[CommunicationsBus::MAX_MESSAGE_LEN];
+	size_t len;
+
+	*retCode = bus.receive(Parser::busId(id, propId), data, len, blocking);
+	if (*retCode) {
+		return typename Parser::result_type();
+	}
+
+	return Parser::parse(id, propId, data, len);
+}
+
 inline int Puck::getPropertyId(enum Property prop, enum PuckType pt, int fwVers)
 		throw(std::runtime_error)
 {
@@ -82,6 +199,52 @@ inline int Puck::getPropertyId(enum Property prop, enum PuckType pt, int fwVers)
 		throw std::runtime_error("Puck::getPropertyId(): Invalid property. Check /var/log/syslog for details.");
 	}
 	return propId;
+}
+
+
+inline int Puck::StandardParser::busId(int id, int propId)
+{
+	return Puck::encodeBusId(id, PuckGroup::FGRP_OTHER);
+}
+
+inline Puck::StandardParser::result_type Puck::StandardParser::parse(int id,
+		int propId, const unsigned char* data, size_t len)
+{
+	bool err = false;
+	if (len != 4 && len != 6) {
+		syslog(
+				LOG_ERR,
+				"%s: expected message length of 4 or 6, got message length of %d",
+				__func__, len);
+		err = true;
+	}
+	if (!(data[0] & Puck::SET_MASK)) {
+		syslog(LOG_ERR, "%s: expected SET command, got GET request", __func__);
+		err = true;
+	}
+	if ((propId & Puck::PROPERTY_MASK) != (data[0] & Puck::PROPERTY_MASK)) {
+		syslog(LOG_ERR, "%s: expected property = %d, got property %d",
+				__func__, (int) (propId & Puck::PROPERTY_MASK), (int) (data[0]
+						& Puck::PROPERTY_MASK));
+		err = true;
+	}
+	if (data[1]) {
+		syslog(LOG_ERR,
+				"%s: expected second data byte to be 0, got value of %d",
+				__func__, (int) data[1]);
+		err = true;
+	}
+
+	if (err) {
+		throw std::runtime_error("Puck::receiveGetPropertyReply(): Unexpected "
+				"message. Check /var/log/syslog for details.");
+	}
+
+	int value = (data[len - 1] & 0x80) ? -1 : 0;
+	for (int i = len - 1; i >= 2; --i) {
+		value = (value << 8) | data[i];
+	}
+	return value;
 }
 
 
--- trunk/src/puck.cpp
+++ trunk/src/puck.cpp
@@ -12,8 +12,8 @@
 #include <unistd.h>
 
 #include <barrett/bus/abstract/communications_bus.h>
-#include <barrett/puck_group.h>
 #include <barrett/puck.h>
+#include <barrett/puck_group.h>
 
 
 namespace barrett {
@@ -76,115 +76,6 @@ void Puck::updateStatus()
 		throw std::runtime_error("Puck::updateStatus(): Bad STAT value. Check /var/log/syslog for details.");
 		break;
 	}
-}
-
-// TODO(dc): throw exception if getProperty is called with a group ID?
-int Puck::getProperty(const CommunicationsBus& bus, int id, int propId)
-{
-	bool successful;
-	int ret = getPropertyHelper(bus, id, propId, true, &successful, 0);
-	if (!successful) {
-		syslog(LOG_ERR, "%s: Puck::receiveGetPropertyReply() returned error %d.", __func__, ret);
-		throw std::runtime_error("Puck::getProperty(): Failed to receive reply. Check /var/log/syslog for details.");
-	}
-	return ret;
-}
-
-int Puck::tryGetProperty(const CommunicationsBus& bus, int id, int propId, bool* successful, int timeout_us)
-{
-	int ret = getPropertyHelper(bus, id, propId, false, successful, timeout_us);
-	if (!(*successful)  &&  ret != 1) {  // some error other than "would block" occurred
-		syslog(LOG_ERR, "%s: Puck::receiveGetPropertyReply() returned error %d.", __func__, ret);
-		throw std::runtime_error("Puck::tryGetProperty(): Receive error. Check /var/log/syslog for details.");
-	}
-	return ret;
-}
-
-int Puck::getPropertyHelper(const CommunicationsBus& bus, int id, int propId,	bool blocking, bool* successful, int timeout_us)
-{
-	int ret = sendGetPropertyRequest(bus, id, propId);
-	if (ret != 0) {
-		syslog(LOG_ERR, "%s: Puck::sendGetPropertyRequest() returned error %d.", __func__, ret);
-		throw std::runtime_error("Puck::getPropertyHelper(): Failed to send request. Check /var/log/syslog for details.");
-	}
-
-	if (timeout_us != 0) {
-		usleep(timeout_us);
-	}
-
-	return receiveGetPropertyReply(bus, id, propId, blocking, successful);
-}
-
-void Puck::setProperty(const CommunicationsBus& bus, int id, int propId, int value)
-{
-	static const size_t MSG_LEN = 6;
-
-	unsigned char data[MSG_LEN];
-	data[0] = (propId & PROPERTY_MASK) | SET_MASK;
-	data[1] = 0;
-	data[2] = (value & 0x000000ff);
-	data[3] = (value & 0x0000ff00) >>  8;
-	data[4] = (value & 0x00ff0000) >> 16;
-	data[5] = (value & 0xff000000) >> 24;
-
-	int ret = bus.send(nodeId2BusId(id), data, MSG_LEN);
-	if (ret != 0) {
-		syslog(LOG_ERR, "%s: Puck::setProperty() returned error %d.", __func__, ret);
-		throw std::runtime_error("Puck::setProperty(): Failed to send SET message. Check /var/log/syslog for details.");
-	}
-}
-
-int Puck::sendGetPropertyRequest(const CommunicationsBus& bus, int id, int propId)
-{
-	static const size_t MSG_LEN = 1;
-
-	unsigned char data[MSG_LEN];
-	data[0] = propId & PROPERTY_MASK;
-
-	return bus.send(nodeId2BusId(id), data, MSG_LEN);
-}
-
-int Puck::receiveGetPropertyReply(const CommunicationsBus& bus, int id, int propId, bool blocking, bool* successful)
-{
-	unsigned char dataIn[CommunicationsBus::MAX_MESSAGE_LEN];
-	size_t lenIn;
-	// TODO(dc): generalize for properties that respond to special groups?
-	int ret = bus.receive(encodeBusId(id, PuckGroup::FGRP_OTHER), dataIn, lenIn, blocking);
-	if (ret) {
-		*successful = false;
-		return ret;
-	} else {
-		*successful = true;
-	}
-
-	bool err = false;
-	if (lenIn != 4  &&  lenIn != 6) {
-		syslog(LOG_ERR, "%s: expected message length of 4 or 6, got message length of %d", __func__, lenIn);
-		err = true;
-	}
-	if ( !(dataIn[0] & SET_MASK) ) {
-		syslog(LOG_ERR, "%s: expected SET command, got GET request", __func__);
-		err = true;
-	}
-	if ((propId & PROPERTY_MASK) != (dataIn[0] & PROPERTY_MASK)) {
-		syslog(LOG_ERR, "%s: expected property = %d, got property %d", __func__, (int)(propId & PROPERTY_MASK), (int)(dataIn[0] & PROPERTY_MASK));
-		err = true;
-	}
-	if (dataIn[1]) {
-		syslog(LOG_ERR, "%s: expected second data byte to be 0, got value of %d", __func__, (int)dataIn[1]);
-		err = true;
-	}
-
-	if (err) {
-		throw std::runtime_error("Puck::receiveGetPropertyReply(): Unexpected message. Check /var/log/syslog for details.");
-	}
-
-
-	int value = (dataIn[lenIn-1] & 0x80) ? -1 : 0;
-	for (int i = lenIn-1; i >= 2; --i) {
-		value = (value << 8) | dataIn[i];
-	}
-	return value;
 }
 
 const char Puck::puckTypeStrs[][12] = { "Monitor", "Safety", "Motor", "ForceTorque", "Unknown" };
