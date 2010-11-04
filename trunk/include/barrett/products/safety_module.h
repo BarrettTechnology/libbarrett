f9232d03ac5f563678101c5965823825aebcdc46
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-11-04 18:23:49 +0000

    Made SafetyModule class to encapsulate safety board and safety puck functionality.
    Added abstract SpecialPuck class to serve as a base for classes like MotorPuck.
    
    Updated MotorPuck to use the SpecialPuck base.

---
+++ trunk/include/barrett/products/safety_module.h
@@ -0,0 +1,52 @@
+/*
+ * safety_module.h
+ *
+ *  Created on: Nov 4, 2010
+ *      Author: dc
+ */
+
+#ifndef BARRETT_PRODUCTS_SAFETY_MODULE_H_
+#define BARRETT_PRODUCTS_SAFETY_MODULE_H_
+
+
+#include <stdexcept>
+
+#include <syslog.h>
+
+#include <barrett/products/puck.h>
+#include <barrett/products/abstract/special_puck.h>
+
+
+namespace barrett {
+
+
+class SafetyModule : public SpecialPuck {
+public:
+	enum SafetyMode {
+		ESTOP, IDLE, ACTIVE
+	};
+
+
+	SafetyModule(Puck* puck = NULL) : SpecialPuck(puck, Puck::PT_Safety) {}
+	virtual ~SafetyModule() {}
+
+	virtual void update() {}
+
+	enum SafetyMode getMode() const {
+		int s = getProperty(Puck::MODE);
+		if (s < 0  ||  s > 2) {
+			syslog(LOG_ERR, "SafetyModule::getMode(): Expected MODE value of 0, 1, or 2. Got value of %d.", s);
+			throw std::runtime_error("SafetyModule::getMode(): Bad MODE value. Check /var/log/syslog for details.");
+		}
+		return static_cast<enum SafetyMode>(s);
+	}
+
+	bool wamIsZeroed() const { return getProperty(Puck::ZERO) == 1; }
+	void setWamZeroed(bool zeroed) const { setProperty(Puck::ZERO, zeroed); }
+};
+
+
+}
+
+
+#endif /* BARRETT_PRODUCTS_SAFETY_MODULE_H_ */
