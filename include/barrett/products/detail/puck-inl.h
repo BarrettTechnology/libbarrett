55510306153c6d27c4494405b8066db779c9f616
Dan Cody <dtydc5@users.noreply.github.com>
2010-11-05 21:29:40 +0000

    Adjusted Puck turn-off-time allowance to 10ms.

--- include/barrett/products/detail/puck-inl.h
+++ include/barrett/products/detail/puck-inl.h
@@ -67,7 +67,7 @@ void Puck::wake(Container<Puck*> pucks)
 		// bus-off. Wait until this Puck drops off the bus before trying to
 		// wake the next one.
 		// TODO(dc): is there a more robust way of doing this?
-		usleep(2000);
+		usleep(10000);
 	}
 
 	usleep(WAKE_UP_TIME);
