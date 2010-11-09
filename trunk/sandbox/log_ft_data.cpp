7697c3416bb55d3f7c57df039c864ffb07cacb95
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-11-09 22:26:17 +0000

    Updated sandbox/log_ft_data.cpp to use the new ForceTorqueSensor class.

--- trunk/sandbox/log_ft_data.cpp
+++ trunk/sandbox/log_ft_data.cpp
@@ -6,31 +6,38 @@
  */
 
 #include <iostream>
-#include <string>
 #include <cstdio>
 #include <cstdlib>
 
-#include <sys/mman.h>
+#include <syslog.h>
 #include <signal.h>
 #include <unistd.h>
-
-#include <boost/thread.hpp>
-#include <boost/tuple/tuple.hpp>
 
 #include <native/task.h>
 #include <native/timer.h>
 
-#include <barrett/cdlbt/bus.h>
-#include <barrett/cdlbt/bus_can.h>
-#include <barrett/log.h>
+#include <boost/thread.hpp>
+#include <boost/tuple/tuple.hpp>
+
 #include <barrett/detail/stacktrace.h>
+#include <barrett/detail/stl_utils.h>
+#include <barrett/units.h>
+#include <barrett/log.h>
+#include <barrett/bus/bus_manager.h>
+#include <barrett/products/force_torque_sensor.h>
+
 
 using namespace barrett;
+using detail::waitForEnter;
+BARRETT_UNITS_FIXED_SIZE_TYPEDEFS;
+
+typedef boost::tuple<double, cf_type, ct_type> tuple_type;
+
+bool g_Going = true;  // Global
 
 
-void waitForEnter() {
-	static std::string line;
-	std::getline(std::cin, line);
+void stopThreads(int sig) {
+	g_Going = false;
 }
 
 void warnOnSwitchToSecondaryMode(int)
@@ -40,40 +47,63 @@ void warnOnSwitchToSecondaryMode(int)
 	std::cerr << "WARNING: Switched out of RealTime. Stack-trace in syslog.\n";
 }
 
-RTIME secondsToRTIME(double s) {
-	return static_cast<RTIME>(s * 1e9);
+void ftThreadEntryPoint(bool* going, double T_s, ForceTorqueSensor& fts, log::RealTimeWriter<tuple_type>* lw, int windowSize, int* numSamples, tuple_type* sum) {
+	tuple_type t;
+
+	rt_task_shadow(new RT_TASK, NULL, 10, 0);
+	rt_task_set_mode(0, T_PRIMARY | T_WARNSW, NULL);
+	rt_task_set_periodic(NULL, TM_NOW, T_s * 1e9);
+
+	RTIME now = rt_timer_read();
+	RTIME lastUpdate = now;
+
+	while (*going) {
+		rt_task_wait_period(NULL);
+		now = rt_timer_read();
+		fts.update(true);  // Do a realtime update (no sleeping while waiting for messages)
+
+		boost::get<0>(t) = ((double) now - lastUpdate) * 1e-9;
+		boost::get<1>(t) = fts.getForce();
+		boost::get<2>(t) = fts.getTorque();
+
+		if (lw != NULL) {
+			lw->putRecord(t);
+		} else {
+			if (*numSamples == 0) {
+				boost::get<1>(*sum).setZero();
+				boost::get<2>(*sum).setZero();
+			}
+			if (*numSamples < windowSize) {
+				boost::get<1>(*sum) += boost::get<1>(t);
+				boost::get<2>(*sum) += boost::get<2>(t);
+				++(*numSamples);
+			}
+		}
+
+		lastUpdate = now;
+	}
+
+	rt_task_set_mode(T_WARNSW, 0, NULL);
 }
 
-void canThread();
-
-double T_s = 0.002;
-const int ID = BT_BUS_PUCK_ID_FT;
-const int CAN_PORT = 1;
-const RTIME FLIGHT_TIME = 75000;
-const int FW_VERS = 148;  // bogus value until the F/T firmware implements VERS
-const int NUM_SENSORS = 6;  // 6 strain gauges, 3 thermistors
-const int NUM_MESSAGES = 2;  // 2 from strain gauges, 3 from the thermistors
-
-bool going = true;
-bool fileMode = false;
-int windowSize, numSamples, numSets = 0;
-math::Vector<NUM_SENSORS>::type sum;
-struct bt_bus_can* dev = NULL;
-
-typedef boost::tuple<double, math::Vector<NUM_SENSORS>::type> tuple_type;
-barrett::log::RealTimeWriter<tuple_type>* lw;
+void showUsageAndExit(const char* programName) {
+	printf("Usage: %s {-f <fileName> | -a <windowSize>} [<samplePeriodInSeconds>]\n", programName);
+	printf("    -f <fileName>    Log data to a file\n");
+	printf("    -a <windowSize>  Print statistics on segments of data\n");
+	exit(0);
+}
 
 
 int main(int argc, char** argv) {
 	char* outFile = NULL;
+	double T_s = 0.002;  // Default: 500Hz
+	bool fileMode = false;
+	int windowSize;
 
 	if (argc == 4) {
 		T_s = std::atof(argv[3]);
 	} else if (argc != 3) {
-		printf("Usage: %s {-f <fileName> | -a <windowSize>} [<samplePeriodInSeconds>]\n", argv[0]);
-		printf("    -f <fileName>    Log data to a file\n");
-		printf("    -a <windowSize>  Print statistics on segments of data\n");
-		return 0;
+		showUsageAndExit(argv[0]);
 	}
 
 	printf("Sample period: %fs\n", T_s);
@@ -84,153 +114,64 @@ int main(int argc, char** argv) {
 	} else if (strcmp(argv[1], "-a") == 0) {
 		fileMode = false;
 		windowSize = atoi(argv[2]);
-		numSamples = windowSize;
 		printf("Window size: %d\n", windowSize);
+	} else {
+		showUsageAndExit(argv[0]);
 	}
 	printf("\n");
 
 
-	mlockall(MCL_CURRENT|MCL_FUTURE);
 	signal(SIGXCPU, &warnOnSwitchToSecondaryMode);
 
-	if (bt_bus_can_create(&dev, CAN_PORT)) {
-		printf("Couldn't open the CAN bus on port %d.\n", CAN_PORT);
-		return -1;
-	}
-
+	char tmpFile[L_tmpnam];
+	log::RealTimeWriter<tuple_type>* lw = NULL;
 	if (fileMode) {
-		printf(">>> Press [Enter] to start collecting data.\n");
-		waitForEnter();
+		signal(SIGINT, &stopThreads);
 
-		char tmpFile[L_tmpnam];
 		if (std::tmpnam(tmpFile) == NULL) {
-			printf("Couldn't create temporary file!\n");
+			printf("ERROR: Couldn't create temporary file!\n");
 			return 1;
 		}
 		lw = new barrett::log::RealTimeWriter<tuple_type>(tmpFile, T_s);
-		boost::thread t(canThread);
+	}
+	int numSamples = windowSize, numSets = 0;
+	tuple_type sum;
 
+	BusManager bm;
+	boost::thread ftThread(ftThreadEntryPoint, &g_Going, T_s, *bm.getForceTorqueSensor(), lw, windowSize, &numSamples, &sum);
 
-		printf(">>> Press [Enter] to stop collecting data.\n");
-		waitForEnter();
-
-		going = false;
-		t.join();
-		delete lw;
-
-		barrett::log::Reader<tuple_type> lr(tmpFile);
-		lr.exportCSV(outFile);
-
-		std::remove(tmpFile);
+	if (fileMode) {
+		printf(">>> Logging data. Press [Ctrl-C] to exit.\n");
 	} else {
-		boost::thread t(canThread);
+		printf(">>> Press [Enter] to start a new sample. Press [Ctrl-C] to exit.\n\n");
+		printf("ID,FX,FY,FZ,TX,TY,TZ");
 
-		printf(">>> Press [Enter] to start a sample.\n");
-//		printf("ID,FX,FY,FZ,TX,TY,TZ,T1,T2,T3\n");
-		printf("ID,FX,FY,FZ,TX,TY,TZ\n");
-
-		while (true) {
+		while (g_Going) {
 			waitForEnter();
 			numSamples = 0;
 			while (numSamples != windowSize) {
 				usleep(100000);
 			}
 
-			sum /= windowSize;
-//			printf("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f", ++numSets, sum[0], sum[1], sum[2], sum[3], sum[4], sum[5], sum[6], sum[7], sum[8]);
-			printf("%d,%f,%f,%f,%f,%f,%f", ++numSets, sum[0], sum[1], sum[2], sum[3], sum[4], sum[5]);
+			boost::get<1>(sum) /= windowSize;
+			boost::get<2>(sum) /= windowSize;
+
+			printf("%d,%f,%f,%f,%f,%f,%f", ++numSets, boost::get<1>(sum)[0], boost::get<1>(sum)[1], boost::get<1>(sum)[2], boost::get<2>(sum)[0], boost::get<2>(sum)[1], boost::get<2>(sum)[2]);
 		}
+	}
+
+	ftThread.join();
+	printf("\n");
+
+	if (fileMode) {
+		delete lw;
+
+		log::Reader<tuple_type> lr(tmpFile);
+		lr.exportCSV(outFile);
+		printf("Output written to %s.\n", outFile);
+
+		std::remove(tmpFile);
 	}
 
 	return 0;
-}
-
-int twoByte2int(unsigned char lsb, unsigned char msb) {
-	int res = ((int)msb << 8)  |  lsb;
-
-	if (res & 0x00008000) {
-		res |= 0xffff0000;
-	}
-
-	return res;
-}
-
-void canThread() {
-	struct bt_bus_properties* plist;
-	int id;
-	long value;
-	unsigned char data[8];
-	tuple_type t;
-
-	rt_task_shadow(new RT_TASK, NULL, 10, 0);
-
-	// init sensor
-	bt_bus_can_set_property(dev, ID, 5, 2);
-	usleep(1000000);
-
-	if (bt_bus_properties_create(&plist, FW_VERS)) {
-		printf("Couldn't create property list.\n");
-		exit(-1);
-	}
-
-	bt_bus_can_set_property(dev, ID, plist->FT, 0);  // tarre
-
-
-	// collect data
-	rt_task_set_mode(0, T_PRIMARY | T_WARNSW, NULL);
-	rt_task_set_periodic(NULL, TM_NOW, secondsToRTIME(T_s));
-
-	RTIME t_0 = rt_timer_read();
-	RTIME t_1 = t_0;
-
-	while (going) {
-		// strain gauges
-		bt_bus_can_async_get_property(dev, ID, plist->FT);
-
-//		// temp sensors
-//		rt_timer_spin(FLIGHT_TIME);
-//		bt_bus_can_async_get_property(dev, ID, plist->T1);
-//		rt_timer_spin(FLIGHT_TIME);
-//		bt_bus_can_async_get_property(dev, ID, plist->T2);
-//		rt_timer_spin(FLIGHT_TIME);
-//		bt_bus_can_async_get_property(dev, ID, plist->T3);
-
-		rt_task_wait_period(NULL);
-
-		t_0 = rt_timer_read();
-		boost::get<0>(t) = ((double) t_0 - t_1) * 1e-9;
-		t_1 = t_0;
-
-		for (int i = 0; i < NUM_MESSAGES; ++i) {
-			bt_bus_can_async_read(dev, &id, NULL, &value, NULL, data, 1, 1);
-
-			if ((id & 0x041F) == 0x040a) {
-				boost::get<1>(t)[0] = twoByte2int(data[0], data[1]);
-				boost::get<1>(t)[1] = twoByte2int(data[2], data[3]);
-				boost::get<1>(t)[2] = twoByte2int(data[4], data[5]);
-			} else if ((id & 0x041F) == 0x040b) {
-				boost::get<1>(t)[3] = twoByte2int(data[0], data[1]);
-				boost::get<1>(t)[4] = twoByte2int(data[2], data[3]);
-				boost::get<1>(t)[5] = twoByte2int(data[4], data[5]);
-			} else {
-				printf("Unexpected CAN message!\n");
-			}
-		}
-
-		if (fileMode) {
-			lw->putRecord(t);
-		} else {
-			if (numSamples == 0) {
-				sum.setConstant(0.0);
-			}
-			if (numSamples < windowSize) {
-				sum += boost::get<1>(t);
-				++numSamples;
-			}
-		}
-	}
-
-	rt_task_set_mode(T_WARNSW, 0, NULL);
-
-	bt_bus_properties_destroy(plist);
 }
