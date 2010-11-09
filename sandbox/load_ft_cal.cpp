1e50d2ff17115650b58c401407aa69341d30b81e
Dan Cody <dtydc5@users.noreply.github.com>
2010-11-09 23:08:42 +0000

    Updated sandbox/load_ft_cal.cpp to use the new ForceTorqueSensor class.

--- sandbox/load_ft_cal.cpp
+++ sandbox/load_ft_cal.cpp
@@ -10,63 +10,34 @@
 #include <cstdlib>
 #include <cstdio>
 
-#include <sys/mman.h>
 #include <unistd.h>
-#include <native/task.h>
+
+#include <barrett/bus/bus_manager.h>
+#include <barrett/products/force_torque_sensor.h>
 
 
 using namespace std;
 using namespace barrett;
 
-const int GM_SIZE = 36;  // the gain matrix is a 6x6 matrix with 36 elements
+const int GM_SIZE = 36;  // The gain matrix is a 6x6 matrix with 36 elements
 
 
 int main(int argc, char** argv) {
-	mlockall(MCL_CURRENT|MCL_FUTURE);
-	rt_task_shadow(new RT_TASK, NULL, 10, 0);
-
-
-	int port = 0;
-	switch (argc) {
-	case 2:
-		printf("No port argument given. Using default.\n");
-		break;
-	case 3:
-		port = atoi(argv[2]);
-		break;
-	default:
-		printf("ERROR: Expected 1 or 2 arguments.\n");
-		return -1;
-		break;
-	}
-
-	printf("Using CAN bus port %d.\n", port);
-	bt_bus_can* dev = NULL;
-	if (bt_bus_can_create(&dev, port)) {
-		printf("Couldn't open the CAN bus.\n");
-		return -1;
+	if (argc != 2) {
+		printf("Usage: %s <calibrationFile>\n", argv[0]);
+		printf("    <calibrationFile>    File containing the gain-matrix for the attached Force-Torque Sensor\n");
+		return 1;
 	}
 
 
-	struct bt_bus_properties* plist;
-	long value = 0;
-
-	// init sensor
-	bt_bus_can_set_property(dev, ID, 5, 2);
-	usleep(1000000);
-
-	bt_bus_can_get_property(dev, ID, 0, &value, NULL, 1);  // get VERS
-	if (value < MIN_FW_VERS) {
-		printf("The F/T Sensor does not have recent firmware. (Actual version = %ld. Required version >= %d.)\n", value, MIN_FW_VERS);
-		exit(-1);
+	BusManager bm;
+	if ( !bm.foundForceTorqueSensor() ) {
+		printf("ERROR: No Force-Torque Sensor found!\n");
+		return 1;
 	}
-	if (bt_bus_properties_create(&plist, value)) {
-		printf("Couldn't create property list.\n");
-		exit(-1);
-	}
+	ForceTorqueSensor& fts = *bm.getForceTorqueSensor();
 
 
-	// load cal
 	int calValue = 0;
 	ifstream cal(argv[1]);
 	for (int i = 0; i < GM_SIZE; ++i) {
@@ -74,15 +45,19 @@ int main(int argc, char** argv) {
 			cal >> calValue;
 			if (calValue < -32768  ||  calValue > 32767) {
 				printf("Calibration file is poorly formated: value out of range: %d.\n", calValue);
-				return -1;
+				return 1;
 			}
-			bt_bus_can_set_property(dev, ID, plist->GM, calValue);
+
+			// TODO(dc): Fix once F/T has working ROLE
+			//fts.setProperty(Puck::GM, calValue);
+			Puck::setProperty(bm, fts.getId(), Puck::getPropertyId(Puck::GM, Puck::PT_ForceTorque, 152), calValue);
+
 			printf(".");
 			fflush(stdout);
 			usleep(1000000);
 		} else {
 			printf("Calibration file is poorly formated: not enough values.\n");
-			return -1;
+			return 1;
 		}
 	}
 
