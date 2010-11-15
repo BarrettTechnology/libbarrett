c8af2078645ba9de9a958237fc9647330dc28b83
Dan Cody <dtydc5@users.noreply.github.com>
2010-11-15 23:10:44 +0000

    Added a SpecialPuck called TactilePuck that wraps a Puck with Tactile Sensors attached. Currently parses only FULL TACT data, not TOP10.
    Updated Hand to use TactilePucks.

--- src/products/hand.cpp
+++ src/products/hand.cpp
@@ -9,11 +9,12 @@
 
 #include <syslog.h>
 
-#include <barrett/products/puck.h>
-#include <barrett/products/motor_puck.h>
-#include <barrett/products/puck_group.h>
-#include <barrett/products/hand.h>
+#include <barrett/detail/stl_utils.h>
 #include <barrett/products/abstract/multi_puck_product.h>
+#include <barrett/products/puck.h>
+#include <barrett/products/puck_group.h>
+#include <barrett/products/tactile_puck.h>
+#include <barrett/products/hand.h>
 
 
 namespace barrett {
@@ -23,11 +24,21 @@ const enum Puck::Property Hand::props[] = { Puck::P, Puck::T };
 
 
 Hand::Hand(const std::vector<Puck*>& _pucks) :
-	MultiPuckProduct(DOF, _pucks, PuckGroup::BGRP_HAND, props, sizeof(props)/sizeof(props[0]), "Hand::Hand()")
+	MultiPuckProduct(DOF, _pucks, PuckGroup::BGRP_HAND, props, sizeof(props)/sizeof(props[0]), "Hand::Hand()"), tactilePucks()
 {
+	// Make TactilePucks
+	int numTact = 0;
+	for (size_t i = 0; i < DOF; ++i) {
+		if (pucks[i]->hasOption(Puck::RO_Tact)) {
+			++numTact;
+			tactilePucks.push_back(new TactilePuck(pucks[i]));
+		}
+	}
+	syslog(LOG_ERR, "  Found %d Tactile arrays", numTact);
 }
 Hand::~Hand()
 {
+	detail::purge(tactilePucks);
 }
 
 
