24a9a8aada0293bd08852f4693f3d094290484ee
Dan Cody <dtydc5@users.noreply.github.com>
2010-11-18 17:59:57 +0000

    Second pass at sandbox/full_system_test.cpp.

--- sandbox/full_system_test.cpp
+++ sandbox/full_system_test.cpp
@@ -7,6 +7,8 @@
 
 #include <vector>
 
+#include <unistd.h>
+
 #include <boost/thread.hpp>
 #include <boost/ref.hpp>
 
@@ -24,7 +26,7 @@ using boost::ref;
 
 
 template<size_t DOF>
-void runWam(const bool& going, BusManager& bm, systems::Wam<DOF>& wam) {
+void moveWam(const bool& going, BusManager& bm, systems::Wam<DOF>& wam) {
 	BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
 
 	while (going) {
@@ -32,15 +34,44 @@ void runWam(const bool& going, BusManager& bm, systems::Wam<DOF>& wam) {
 	}
 }
 
-void runHand(const bool& going, Hand& hand) {
+void moveHand(const bool& going, Hand& hand) {
+	Hand::jp_type jp0, jp1;
+	jp0.setZero();
+	jp1 << 30000, 30000, 30000, 10000;
+
 	while (going) {
-		sleep(1);
+		sleep(3);
+		hand.trapezoidalMove(jp1, false);
+		sleep(3);
+		hand.trapezoidalMove(jp0, false);
+
+//		hand.updatePosition();
+//		hand.updateStrain();
+//		std::cout << hand.getPosition() << " [" << hand.getStrain()[0] << "," << hand.getStrain()[1] << "," << hand.getStrain()[2] << "," << hand.getStrain()[3] << "]" << "\n";
+	}
+
+	hand.idle();
+}
+
+void readHand(const bool& going, Hand& hand) {
+	while (going) {
+		usleep(10000);
+		hand.updatePosition();
+		hand.updateStrain();
 	}
 }
 
-void runFTS(const bool& going, ForceTorqueSensor& fts) {
+void readTact(const bool& going, Hand& hand) {
 	while (going) {
-		sleep(1);
+		usleep(10000);
+		hand.updateTactFull();
+	}
+}
+
+void readFTS(const bool& going, ForceTorqueSensor& fts) {
+	while (going) {
+		usleep(10000);
+		fts.update();
 	}
 }
 
@@ -55,29 +86,37 @@ int main(int argc, char** argv) {
 
 
 	// WAM
-//	bm.waitForWam();
+	bm.waitForWam();
 	if (bm.foundWam4()) {
-		threads.push_back(new boost::thread(runWam<4>, ref(going), ref(bm), ref(*bm.getWam4())));
-		printf(">>> Starting WAM4...\n");
+		printf("Starting WAM4...\n");
+		threads.push_back(new boost::thread(moveWam<4>, ref(going), ref(bm), ref(*bm.getWam4(false))));
 	} else if (bm.foundWam7()) {
-		threads.push_back(new boost::thread(runWam<7>, ref(going), ref(bm), ref(*bm.getWam7())));
-		printf(">>> Starting WAM7...\n");
+		printf("Starting WAM7...\n");
+		threads.push_back(new boost::thread(moveWam<7>, ref(going), ref(bm), ref(*bm.getWam7(false))));
 	} else {
 		printf(">>> WARNING: No WAM found\n");
 	}
 
 	// Hand
 	if (bm.foundHand()) {
-		threads.push_back(new boost::thread(runHand, ref(going), ref(*bm.getHand())));
-		printf(">>> Starting Hand...\n");
+		printf("Starting Hand...\n");
+		Hand& hand = *bm.getHand();
+
+		printf(">>> Press [Enter] to initialize Hand. (Make sure it has room!)\n");
+		waitForEnter();
+		hand.initialize();
+
+		threads.push_back(new boost::thread(moveHand, ref(going), ref(hand)));
+		threads.push_back(new boost::thread(readHand, ref(going), ref(hand)));
+		threads.push_back(new boost::thread(readTact, ref(going), ref(hand)));
 	} else {
 		printf(">>> WARNING: No Hand found\n");
 	}
 
 	// F/T
 	if (bm.foundForceTorqueSensor()) {
-		threads.push_back(new boost::thread(runFTS, ref(going), ref(*bm.getForceTorqueSensor())));
-		printf(">>> Starting F/T Sensor...\n");
+		printf("Starting F/T Sensor...\n");
+		threads.push_back(new boost::thread(readFTS, ref(going), ref(*bm.getForceTorqueSensor())));
 	} else {
 		printf(">>> WARNING: No F/T Sensor found\n");
 	}
