70729f618cbd4ddfaea0a20a006cf4b135099bd8
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2010-12-28 15:52:41 +0000

    Improved configuration options for the standard main().

--- trunk/include/barrett/standard_main_function.h
+++ trunk/include/barrett/standard_main_function.h
@@ -18,6 +18,14 @@
 	template<size_t DOF> int wam_main(int argc, char** argv, ::barrett::BusManager& bm, ::barrett::systems::Wam<DOF>& wam);
 #endif
 
+#ifndef BARRETT_SMF_WAIT_FOR_SHIFT_ACTIVATE
+#  define BARRETT_SMF_WAIT_FOR_SHIFT_ACTIVATE true
+#endif
+
+#ifndef BARRETT_SMF_WAM_CONFIG_PATH
+#  define BARRETT_SMF_WAM_CONFIG_PATH NULL
+#endif
+
 
 int main(int argc, char** argv) {
 	// Give us pretty stack-traces when things die
@@ -28,9 +36,9 @@ int main(int argc, char** argv) {
 	bm.wakeAllPucks();
 
 	if (bm.foundWam4()) {
-		return wam_main(argc, argv, bm, *bm.getWam4());
+		return wam_main(argc, argv, bm, *bm.getWam4(BARRETT_SMF_WAIT_FOR_SHIFT_ACTIVATE, BARRETT_SMF_WAM_CONFIG_PATH));
 	} else if (bm.foundWam7()) {
-		return wam_main(argc, argv, bm, *bm.getWam7());
+		return wam_main(argc, argv, bm, *bm.getWam7(BARRETT_SMF_WAIT_FOR_SHIFT_ACTIVATE, BARRETT_SMF_WAM_CONFIG_PATH));
 	} else {
 		printf(">>> ERROR: No WAM was found. Perhaps you have found a bug in BusManager::waitForWam().\n");
 		return 1;
