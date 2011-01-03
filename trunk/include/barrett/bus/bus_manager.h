b6a7822fab54fe1c0ee983dc3b8afe971d5b72a0
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2011-01-03 23:13:00 +0000

    Changed BusManager to allow wrapping of any type of CommunicationsBus (default is CANSocket).

--- trunk/include/barrett/bus/bus_manager.h
+++ trunk/include/barrett/bus/bus_manager.h
@@ -17,7 +17,6 @@
 #include <barrett/detail/ca_macro.h>
 #include <barrett/thread/abstract/mutex.h>
 #include <barrett/bus/abstract/communications_bus.h>
-#include <barrett/bus/can_socket.h>
 
 
 namespace barrett {
@@ -25,35 +24,34 @@ namespace barrett {
 
 class BusManager : public CommunicationsBus {
 public:
-	BusManager();
+	BusManager(CommunicationsBus* bus = NULL);
 	BusManager(int port);
 	virtual ~BusManager();
 
-	const CommunicationsBus& getUnderlyingBus() const { return bus; }
-	virtual thread::Mutex& getMutex() const { return bus.getMutex(); }
+	const CommunicationsBus& getUnderlyingBus() const { return *bus; }
+	virtual thread::Mutex& getMutex() const { return bus->getMutex(); }
 
-	virtual void open(int port) { bus.open(port); }
-	virtual void close() { bus.close(); }
-	virtual bool isOpen() { return bus.isOpen(); }
+	virtual void open(int port) { bus->open(port); }
+	virtual void close() { bus->close(); }
+	virtual bool isOpen() { return bus->isOpen(); }
 
 	virtual int send(int busId, const unsigned char* data, size_t len) const
-		{ return bus.send(busId, data, len); }
+		{ return bus->send(busId, data, len); }
 	virtual int receive(int expectedBusId, unsigned char* data, size_t& len,
 			bool blocking = true, bool realtime = false) const;
 	virtual int receiveRaw(int& busId, unsigned char* data, size_t& len,
 			bool blocking = true) const
-		{ return bus.receiveRaw(busId, data, len, blocking); }
+		{ return bus->receiveRaw(busId, data, len, blocking); }
 
 protected:
 	int updateBuffers() const;
 	void storeMessage(int busId, const unsigned char* data, size_t len) const;
 	bool retrieveMessage(int busId, unsigned char* data, size_t& len) const;
 
-	CommunicationsBus& bus;
+	CommunicationsBus* bus;
+	bool deleteBus;
 
 private:
-	typedef CANSocket ActualBusType;
-
 	struct Message {
 		Message(const unsigned char* d, size_t l) :
 			len(l)
@@ -66,7 +64,7 @@ private:
 			memcpy(d, data, len);
 		}
 
-		unsigned char data[ActualBusType::MAX_MESSAGE_LEN];
+		unsigned char data[CommunicationsBus::MAX_MESSAGE_LEN];
 		size_t len;
 	};
 
@@ -77,7 +75,6 @@ private:
 			boost::circular_buffer<Message>(MESSAGE_BUFFER_SIZE) {}
 	};
 
-	ActualBusType actualBus;
 	mutable std::map<int, MessageBuffer> messageBuffers;
 
 	DISALLOW_COPY_AND_ASSIGN(BusManager);
