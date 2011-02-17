ac636c0e8616c5a31f6660500d2ca2850a1aa8f7
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2011-02-17 02:15:47 +0000

    Modified sandbox/safety_module.cpp to show how to recover from an E-stop.

--- trunk/sandbox/safety_module.cpp
+++ trunk/sandbox/safety_module.cpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 
 #include <barrett/products/product_manager.h>
+#include <barrett/systems.h>
 
 
 using namespace barrett;
@@ -21,23 +22,39 @@ int main() {
 	ProductManager pm;
 	SafetyModule& sm = *pm.getSafetyModule();
 
-	printCurrentMode(sm);
-
-	sm.waitForMode(SafetyModule::IDLE);
-	if ( !pm.foundWam() ) {
-		pm.enumerate();
-		if ( !pm.foundWam() ) {
-			printf("ERROR: No WAM was found.\n");
-			return 1;
-		}
-	}
-
-	systems::Wam<4>& wam = *pm.getWam4();
-	sm.waitForMode(SafetyModule::ACTIVE);
-
 	while (true) {
-		sm.waitForModeChange();
 		printCurrentMode(sm);
+
+		// Tell the ExecutionManager to do nothing when it detects an ESTOP. (The default
+		// default behavior is to terminate the program.) This must be done after every ESTOP
+		// because ProductManager::cleanUpAfterEstop() causes the ExecutionManager to be destroyed.
+		// A new ExecutionManager is constructed in this call.
+		pm.getExecutionManager()->clearErrorCallback();
+
+		// An ESTOP that occurs in between a call to ProductManager::waitForWam() and a call to
+		// ProductManager::getWam*() might cause Puck::wake() (which is called from the LowLevelWam
+		// constructor) to throw a std::runtime_error exception.
+		pm.waitForWam();
+
+		// Instantiate a Wam object and start the realtime control loop
+		if (pm.foundWam4()) {
+			pm.getWam4();
+		} else {
+			pm.getWam7();
+		}
+
+		// An ESTOP that occurs while executing the following line will cause the program to hang
+		// waiting for a Shift-activate that will never come...
+		sm.waitForMode(SafetyModule::ACTIVE);
+
+		while (true) {
+			if (sm.waitForModeChange() == SafetyModule::ESTOP) {
+				pm.cleanUpAfterEstop();
+				break;
+			}
+
+			printCurrentMode(sm);
+		}
 	}
 
 	return 0;
