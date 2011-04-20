d1d1839feb44df56913b04c4a4301320b9c010d4
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2011-04-20 22:44:19 +0000

    Added logging functionality to sandbox/log_temp_data.cpp.

--- trunk/sandbox/log_temp_data.cpp
+++ trunk/sandbox/log_temp_data.cpp
@@ -5,24 +5,80 @@
  *      Author: dc
  */
 
+#include <cstdio>
 #include <vector>
+
+#include <native/timer.h>
+
+#include <boost/thread.hpp>
+#include <boost/array.hpp>
 
 #include <barrett/units.h>
 #include <barrett/systems.h>
+#include <barrett/log.h>
 #include <barrett/products/product_manager.h>
 
+#define BARRETT_SMF_VALIDATE_ARGS
+//#define BARRETT_SMF_DONT_WAIT_FOR_SHIFT_ACTIVATE
 #include <barrett/standard_main_function.h>
 
 
 using namespace barrett;
 
 
+bool validate_args(int argc, char** argv) {
+	if (argc != 2) {
+		printf("Usage: %s <fileName>\n", argv[0]);
+		printf("  fileName:  File to record temperature data to\n");
+		return false;
+	} else {
+		return true;
+	}
+}
+
+void logEntryPoint(const PuckGroup& pg, const char* outFile) {
+	const size_t MAX_PG_SIZE = 11;
+	typedef boost::tuple<double, boost::array<int, MAX_PG_SIZE>, boost::array<int, MAX_PG_SIZE> > tuple_type;
+
+	char tmpFile[L_tmpnam];
+	if (std::tmpnam(tmpFile) == NULL) {
+		printf("ERROR: Couldn't create temporary file!\n");
+		exit(1);
+	}
+
+	// Become a shadowed Xenomai task so we can use rt_timer services.
+	pg.getPucks()[0]->getProperty(Puck::STAT);
+	RTIME start = rt_timer_read();
+
+	tuple_type data;
+	log::Writer<tuple_type> logWriter(tmpFile);
+	while ( !boost::this_thread::interruption_requested() ) {
+		boost::get<0>(data) = (rt_timer_read() - start) * 1e-9;
+		pg.getProperty(Puck::TEMP, boost::get<1>(data).data());
+		pg.getProperty(Puck::THERM, boost::get<2>(data).data());
+		logWriter.putRecord(data);
+
+		usleep(5000000);
+	}
+	logWriter.close();
+
+	log::Reader<tuple_type> logReader(tmpFile);
+	logReader.exportCSV(outFile);
+	printf("Output written to %s.\n", outFile);
+	std::remove(tmpFile);
+}
+
 template<size_t DOF>
 int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) {
 	BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
+//	pm.getExecutionManager()->stop();
 
 	// Disable torque saturation
 	wam.jpController.setControlSignalLimit(v_type(0.0));
+
+
+	boost::thread logThread(logEntryPoint, boost::ref(wam.llww.getLowLevelWam().getGroup()), argv[1]);
+	printf("Recording!\n");
 
 
 	jp_type tmp;
@@ -96,4 +152,9 @@ int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) 
 	// Wait for the user to press Shift-idle
 	pm.getSafetyModule()->waitForMode(SafetyModule::IDLE);
 	return 0;
+
+
+	printf("Recording stopped.\n");
+	logThread.interrupt();
+	logThread.join();
 }
