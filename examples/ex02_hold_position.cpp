8ab9bffb9d9d381b4338e3c14c50c329c86b41dd
Dan Cody <dtydc5@users.noreply.github.com>
2011-05-02 19:48:43 +0000

    Worked on ex02 and ex03.

--- examples/ex02_hold_position.cpp
+++ examples/ex02_hold_position.cpp
@@ -13,6 +13,15 @@
 using namespace barrett;
 
 
+void printMenu() {
+	printf("Commands:\n");
+	printf("  j  Hold joint positions\n");
+	printf("  p  Hold tool position (in Cartesian space)\n");
+	printf("  o  Hold tool orientation\n");
+	printf("  i  Idle (release position/orientation constraints)\n");
+	printf("  q  Quit\n");
+}
+
 template<size_t DOF>
 int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) {
 	BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
@@ -22,12 +31,7 @@ int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) 
 
 	systems::ExposedOutput<Eigen::Quaterniond> toSetPoint;
 
-	printf("Commands:\n");
-	printf("  j  Hold joint positions\n");
-	printf("  p  Hold tool position (in Cartesian space)\n");
-	printf("  o  Hold tool orientation\n");
-	printf("  i  Idle (release position/orientation constraints)\n");
-	printf("  q  Quit\n");
+	printMenu();
 
 	std::string line;
 	bool going = true;
@@ -37,28 +41,37 @@ int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) 
 
 		switch (line[0]) {
 		case 'j':
+			printf("Holding joint positions.\n");
 			wam.moveTo(wam.getJointPositions());
 			break;
 
 		case 'p':
+			printf("Holding tool position.\n");
 			wam.moveTo(wam.getToolPosition());
 			break;
 
 		case 'o':
+			printf("Holding tool orientation.\n");
 			toSetPoint.setValue(wam.getToolOrientation());
 			wam.trackReferenceSignal(toSetPoint.output);
 			break;
 
 		case 'i':
+			printf("WAM idled.\n");
 			wam.idle();
 			break;
 
 		case 'q':
 		case 'x':
+			printf("Quitting.\n");
 			going = false;
 			break;
 
 		default:
+			if (line.size() != 0) {
+				printf("Unrecognized option.\n");
+				printMenu();
+			}
 			break;
 		}
 	}
