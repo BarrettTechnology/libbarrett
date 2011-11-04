b04f2c49202a54a08eb3c6efa466d21a7d350f5e
Dan Cody <dtydc5@users.noreply.github.com>
2011-11-04 22:13:14 +0000

    Added Hand::update() to allow updating several sensors simultaneously. This is slightly more efficient than the Hand::update*() family. Also, having a single update() function is more consistent with LowLevelWam and ForceTorqueSensor.
    Modified sandbox/hand.cpp to take timing data on the new function.

--- sandbox/hand.cpp
+++ sandbox/hand.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <native/timer.h>
+
 #include <barrett/products/product_manager.h>
 
 
@@ -23,26 +25,65 @@ int main() {
 
 
 	hand.initialize();
+	RTIME start, end;
+	const size_t COUNT = 1000;
+
+
+#define PRE \
+	start = rt_timer_read(); \
+	for (size_t i = 0; i < COUNT; ++i) {
+
+#define POST \
+	} \
+	end = rt_timer_read(); \
+	std::cout << (end - start) * 1e-6 / COUNT << "\n";
+
+
+	PRE
+	hand.updatePosition(true);
+	hand.updateStrain(true);
+	hand.updateTactFull(true);
+	POST
+
+	PRE
+	hand.updatePosition();
+	hand.updateStrain();
+	hand.updateTactFull();
+	POST
+
+	PRE
+	hand.update(Hand::S_ALL, true);
+	POST
+
+	PRE
+	hand.update();
+	POST
+
+
+	return 0;
 
 	Hand::jp_type jp;
 	jp.setConstant(0.5);
 	hand.trapezoidalMove(jp);
 
-	hand.updatePosition();
-	hand.updateStrain();
+	hand.updatePosition(true);
+	hand.updateStrain(true);
+	//hand.update(Hand::S_POSITION | Hand::S_FINGER_TIP_TORQUE, true);
 	std::cout << hand.getInnerLinkPosition() << hand.getOuterLinkPosition() << " [" << hand.getStrain()[0] << "," << hand.getStrain()[1] << "," << hand.getStrain()[2] << "," << hand.getStrain()[3] << "]" << "\n";
 
 	Hand::jv_type jv(0.5);
 	hand.setVelocity(jv);
 
 	sleep(1);
-	hand.updatePosition();
-	hand.updateStrain();
+	hand.updatePosition(true);
+	hand.updateStrain(true);
+	//hand.update(Hand::S_POSITION | Hand::S_FINGER_TIP_TORQUE, true);
 	std::cout << hand.getInnerLinkPosition() << hand.getOuterLinkPosition() << " [" << hand.getStrain()[0] << "," << hand.getStrain()[1] << "," << hand.getStrain()[2] << "," << hand.getStrain()[3] << "]" << "\n";
 
 	sleep(1);
-	hand.updatePosition();
-	hand.updateStrain();
+	hand.updatePosition(true);
+	hand.updateStrain(true);
+	//hand.update(Hand::S_POSITION | Hand::S_FINGER_TIP_TORQUE, true);
 	std::cout << hand.getInnerLinkPosition() << hand.getOuterLinkPosition() << " [" << hand.getStrain()[0] << "," << hand.getStrain()[1] << "," << hand.getStrain()[2] << "," << hand.getStrain()[3] << "]" << "\n";
 
 	hand.idle();
