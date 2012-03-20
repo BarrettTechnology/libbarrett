8acc88eee53b7b1bb4c9dae22e6a26a97dce5bfe
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2012-03-20 21:46:14 +0000

    Change list:   * Added wamdiscover utility to help identify WAMs and WAM-PCs on the local network   * Added cmake option "-DWITH_PYTHON=ON" to build shared library with python bindings   * Added python bindings for the barrett::bus namespace. Known issues:   * The wamdiscover utility requires Python >=2.7, or separate installation of argparse module   * The bus::CommunicationsBus::getMutex() method does not have a python binding

---
+++ branches/1.x/src/python.cpp
@@ -0,0 +1,128 @@
+/*
+	Copyright 2011, 2012 Barrett Technology <support@barrett.com>
+
+	This file is part of libbarrett.
+
+	This version of libbarrett is free software: you can redistribute it
+	and/or modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This version of libbarrett is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this version of libbarrett.  If not, see
+	<http://www.gnu.org/licenses/>.
+
+	Further, non-binding information about licensing is available at:
+	<http://wiki.barrett.com/libbarrett/wiki/LicenseNotes>
+*/
+
+/*
+ * python.cpp
+ *
+ *  Created on: Oct 20, 2011
+ *      Author: dc
+ */
+
+
+#include <string>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/python.hpp>
+
+#include <barrett/bus/can_socket.h>
+
+#include <barrett/products/puck.h>
+#include <barrett/products/product_manager.h>
+
+#include <barrett/systems/real_time_execution_manager.h>
+#include <barrett/systems/wam.h>
+
+
+using namespace barrett;
+using namespace boost::python;
+
+
+// Forward declarations of the "build functions"
+void pythonBusInterface();
+
+
+class Namespace {};
+void makeNamespace(const char* name, void(&buildFunction)()) {
+	scope s = class_<Namespace, boost::noncopyable>(name, no_init);
+	buildFunction();
+}
+
+
+bool isWamActivated(ProductManager& pm) {
+	return pm.getSafetyModule()->getMode() == SafetyModule::ACTIVE;
+}
+
+int getPropertyNoRT(const Puck& p, enum Puck::Property prop) {
+	return p.getProperty(prop, false);
+}
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getExecutionManager_overloads, getExecutionManager, 0, 2)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_waitForWam_overloads, waitForWam, 0, 1)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getWam4_overloads, getWam4, 0, 2)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getWam7_overloads, getWam7, 0, 2)
+
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(Wam_gravityCompensate_overloads, gravityCompensate, 0, 1)
+template<size_t DOF>
+void wrapWam() {
+	std::string name = "Wam" + boost::lexical_cast<std::string>(DOF);
+	class_<systems::Wam<DOF>, boost::noncopyable>(name.c_str(), no_init)
+		.def("gravityCompensate", &systems::Wam<DOF>::gravityCompensate,
+				Wam_gravityCompensate_overloads())
+	;
+}
+
+
+BOOST_PYTHON_MODULE(libbarrett)
+{
+	makeNamespace("bus", pythonBusInterface);
+
+
+	// WARNING! The python wrappers below are experimental. They are partially
+	// implemented and are likely to have API changes in the near future.
+
+	// Puck class
+	{
+		// The Puck class becomes the active scope until puckScope is destroyed.
+		scope puckScope = class_<Puck>("Puck", init<bus::CANSocket&, int>()[with_custodian_and_ward<1,2>()])
+			.def("wake", (void(Puck::*)()) &Puck::wake)  // cast to resolve the overload
+			.def("getProperty", getPropertyNoRT)
+		;
+
+		enum_<enum Puck::Property> propEnum("Property");
+		for (int i = 0; i < Puck::NUM_PROPERTIES; ++i) {
+			enum Puck::Property p = (enum Puck::Property)i;
+			propEnum.value(Puck::getPropertyStr(p), p);
+		}
+		propEnum.export_values();
+	}
+
+	class_<systems::RealTimeExecutionManager, boost::noncopyable>("RealTimeExecutionManager", no_init);
+	wrapWam<4>();
+	wrapWam<7>();
+
+	def("isWamActivated", &isWamActivated);
+
+	class_<ProductManager, boost::noncopyable>("ProductManager")
+		.def("getExecutionManager", &ProductManager::getExecutionManager,
+				ProductManager_getExecutionManager_overloads()[return_internal_reference<>()])
+		.def("waitForWam", &ProductManager::waitForWam, ProductManager_waitForWam_overloads())
+		.def("wakeAllPucks", &ProductManager::wakeAllPucks)
+		.def("foundWam7", &ProductManager::foundWam7)
+		.def("getWam7", &ProductManager::getWam7,
+				ProductManager_getWam7_overloads()[return_internal_reference<>()])
+		.def("foundWam4", &ProductManager::foundWam4)
+		.def("getWam4", &ProductManager::getWam4,
+				ProductManager_getWam4_overloads()[return_internal_reference<>()])
+	;
+}
---
+++ branches/1.x/src/python_interface_test.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+"""
+These tests assume that there is a well-behaved, Shift-idled WAM attached to the
+CANbus on CAN_PORT.
+"""
+
+
+from libbarrett import *
+
+
+CAN_PORT = 0
+
+
+def assertHasattrs(obj, attrs):
+	if isinstance(attrs, str):
+		attrs = attrs.split()
+	for a in attrs:
+		assert hasattr(obj, a), "Object \"%s\" doesn't have attr \"%s\"" % (str(obj), a)
+
+
+### bus
+assertHasattrs(bus, "CommunicationsBus CANSocket")
+
+# CommunicationsBus
+assert bus.CommunicationsBus.MAX_MESSAGE_LEN == 8
+assert bus.CommunicationsBus.TIMEOUT > 0
+assertHasattrs(bus.CommunicationsBus, "MAX_MESSAGE_LEN TIMEOUT open close isOpen send send receiveRaw")
+
+try:
+	cb = bus.CommunicationsBus()
+except RuntimeError:
+	pass
+else:
+	assert False, "Shouldn't be able to instantiate bus.CommunicationsBus"
+
+# CANSocket
+assert issubclass(bus.CANSocket, bus.CommunicationsBus)
+c = bus.CANSocket()
+assert not c.isOpen()
+c.open(CAN_PORT)
+assert c.isOpen()
+c.close()
+assert not c.isOpen()
+
+c = bus.CANSocket(CAN_PORT)
+assert c.isOpen()
+c.close()
+assert not c.isOpen()
+
+# BusManager
+assert issubclass(bus.BusManager, bus.CommunicationsBus)
+b = bus.BusManager()
+assert not b.isOpen()
+assert isinstance(b.getUnderlyingBus(), bus.CANSocket)
+
+bus.CANSocket(CAN_PORT)
+b = bus.BusManager(c)
+# TODO(dc): Is there a way to access the pointer of the C++ object?
+#assert b.getUnderlyingBus() is c
+b.close()
+
+b = bus.BusManager(CAN_PORT)
+assert b.isOpen()
+assert b.getUnderlyingBus().isOpen()
+
+try:
+	b.send(0x001, range(bus.CommunicationsBus.MAX_MESSAGE_LEN + 1))
+except RuntimeError:
+	pass
+else:
+	assert False, "Message should have been too long"
+try:
+	b.send(0x001, [5,0,256,0,0,0,0,0])
+except OverflowError:
+	pass
+else:
+	assert False, "Data entry should have been too big"
+b.send(0x001, [3,0,0,0,0,0,0,0])
+assert b.receive(0x426) == [0x83,0,1,0]
+assert b.receive(0x426, False) == []  # We don't expect any messages
+b.send(0x001, [3,0,0,0,0,0,0,0])
+assert b.receiveRaw() == (0x426, [0x83,0,1,0])
+assert b.receiveRaw(False) == (0, [])  # We don't expect any messages
+b.close()
+
+
+
+
+
+
+#p = Puck(bus.CANSocket(CAN_PORT), 1)
+
+#print p.getProperty(Puck.STAT)
