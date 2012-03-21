baffb72f31384d9a916a1106c712de897118a61e
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2012-03-21 13:53:54 +0000

    Extracted python bindings for Puck into src/products/python.cpp. Wrapped Puck enumerations and associated static methods.

--- trunk/src/python_interface_test.py
+++ trunk/src/python_interface_test.py
@@ -18,6 +18,13 @@ def assertHasattrs(obj, attrs):
 	for a in attrs:
 		assert hasattr(obj, a), "Object \"%s\" doesn't have attr \"%s\"" % (str(obj), a)
 
+def shouldRaise(errorType, func, *args, **kwargs):
+	try:
+		func(*args, **kwargs)
+	except errorType:
+		pass
+	else:
+		assert False
 
 ### bus
 assertHasattrs(bus, "CommunicationsBus CANSocket")
@@ -26,13 +33,8 @@ assertHasattrs(bus, "CommunicationsBus CANSocket")
 assert bus.CommunicationsBus.MAX_MESSAGE_LEN == 8
 assert bus.CommunicationsBus.TIMEOUT > 0
 assertHasattrs(bus.CommunicationsBus, "MAX_MESSAGE_LEN TIMEOUT open close isOpen send send receiveRaw")
-
-try:
-	cb = bus.CommunicationsBus()
-except RuntimeError:
-	pass
-else:
-	assert False, "Shouldn't be able to instantiate bus.CommunicationsBus"
+#Shouldn't be able to instantiate bus.CommunicationsBus
+shouldRaise(RuntimeError, bus.CommunicationsBus)
 
 # CANSocket
 assert issubclass(bus.CANSocket, bus.CommunicationsBus)
@@ -64,18 +66,10 @@ b = bus.BusManager(CAN_PORT)
 assert b.isOpen()
 assert b.getUnderlyingBus().isOpen()
 
-try:
-	b.send(0x001, range(bus.CommunicationsBus.MAX_MESSAGE_LEN + 1))
-except RuntimeError:
-	pass
-else:
-	assert False, "Message should have been too long"
-try:
-	b.send(0x001, [5,0,256,0,0,0,0,0])
-except OverflowError:
-	pass
-else:
-	assert False, "Data entry should have been too big"
+# Message is too long
+shouldRaise(RuntimeError, bus.BusManager.send, b, 0x001, range(bus.CommunicationsBus.MAX_MESSAGE_LEN + 1))
+# 256 doesn't fit in a char
+shouldRaise(OverflowError, bus.BusManager.send, b, 0x001, [5,0,256,0,0,0,0,0])
 b.send(0x001, [0,0,0,0,0,0,0,0])  # Can we send an 8-byte message?
 
 b.send(0x001, [3])
@@ -87,10 +81,32 @@ assert b.receiveRaw(False) == (0, [])  # We don't expect any messages
 b.close()
 
 
+### products
 
+# Puck
+assertHasattrs(Puck, "RO_MagEncOnSerial RO_Strain RO_OpticalEncOnEnc")
 
+assertHasattrs(Puck, "PT_Monitor PT_Motor PT_Unknown")
+assert Puck.getPuckTypeStr(Puck.PT_Safety) == "Safety"
+assert Puck.getPuckTypeStr(Puck.PT_Motor) == "Motor"
+
+assertHasattrs(Puck, "CMD ID JP MODE SAVE TSTOP X6")
+assert Puck.getPropertyStr(Puck.STAT) == "STAT"
+assert Puck.getPropertyStr(Puck.FET0) == "FET0"
+# Puck.getPropertyStr() should reject invalid properties
+#shouldRaise(RuntimeError, Puck.getPropertyStr, Puck.Property(-1))
+
+assert Puck.getPropertyEnum("MODE") == Puck.MODE
+assert Puck.getPropertyEnum("TStoP") == Puck.TSTOP
+shouldRaise(ValueError, Puck.getPropertyEnum, "")
+shouldRaise(ValueError, Puck.getPropertyEnum, "myprop")
+
+assert Puck.getPropertyEnumNoThrow("vl1") == Puck.VL1
+assert Puck.getPropertyEnumNoThrow("asdfasdf") == -1
 
 
 #p = Puck(bus.CANSocket(CAN_PORT), 1)
 
 #print p.getProperty(Puck.STAT)
+
+print "Success! All tests passed."
