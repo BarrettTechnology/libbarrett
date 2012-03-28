900eaf7af39e00797eeb51e4e15f4556e8a45fa0
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2012-03-28 22:05:52 +0000

    Change list:   * Added auto-recovery features that reduce the number of failures on start-up due to CANbus errors   * Improved support for controlling two WAM systems from one PCI CAN card   * Fixed a bug that prevented multiple systems::RealTimeExecutionManager's from running realtime loops concurrently   * Improved the diagnostic messages printed to /var/log/syslog by systems::RealTimeExecutionManager Known issues:   * The CANbus auto-recover feature causes a one-time kernel assertion failure. (This is believed to be an issue with Xenomai 2.5.5.2.)

--- branches/1.x/src/systems/real_time_execution_manager.cpp
+++ branches/1.x/src/systems/real_time_execution_manager.cpp
@@ -1,4 +1,27 @@
 /*
+	Copyright 2009, 2010, 2011, 2012 Barrett Technology <support@barrett.com>
+
+	This file is part of libbarrett.
+
+	This version of libbarrett is free software: you can redistribute it
+	and/or modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This version of libbarrett is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this version of libbarrett.  If not, see
+	<http://www.gnu.org/licenses/>.
+
+	Further, non-binding information about licensing is available at:
+	<http://wiki.barrett.com/libbarrett/wiki/LicenseNotes>
+*/
+
+/*
  * real_time_execution_manager.cpp
  *
  *  Created on: Dec 11, 2009
@@ -6,16 +29,19 @@
  */
 
 #include <stdexcept>
+#include <string>
 #include <limits>
 #include <cmath>
 
 #include <syslog.h>
+#include <errno.h>
 #include <sys/mman.h>
 
 #include <native/task.h>
 #include <native/timer.h>
 
 #include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <barrett/thread/real_time_mutex.h>
 #include <barrett/systems/abstract/execution_manager.h>
@@ -27,7 +53,6 @@ namespace systems {
 
 
 // TODO(dc): test!
-// TODO(dc): check return codes for errors!
 
 
 namespace detail {
@@ -45,24 +70,34 @@ void rtemEntryPoint(void* cookie)
 	RealTimeExecutionManager* rtem = reinterpret_cast<RealTimeExecutionManager*>(cookie);
 
 	uint32_t period_us = rtem->period * 1e6;
-    RTIME start;
-    uint32_t duration;
+	RTIME start;
+	uint32_t duration;
 	uint32_t min = std::numeric_limits<unsigned int>::max();
 	uint32_t max = 0;
 	uint64_t sum = 0;
 	uint64_t sumSq = 0;
 	uint32_t loopCount = 0;
-	uint32_t missedCycleCount = 0;
+	uint32_t overruns = 0;
+	uint32_t missedReleasePoints = 0;
+	unsigned long newMissedReleasePoints = 0;
 
+	int ret;
 
-	rt_task_set_periodic(NULL, TM_NOW, secondsToRTIME(rtem->period));
+	ret = rt_task_set_periodic(NULL, TM_NOW, secondsToRTIME(rtem->period));
+	if (ret != 0) {
+		syslog(LOG_ERR, "%s: rt_task_set_periodic(): (%d) %s\n", __func__, -ret, strerror(-ret));
+		exit(2);
+	}
 
 	rtem->running = true;
 	try {
 		while ( !rtem->stopRunning ) {
-			rt_task_wait_period(NULL);
-
-
+			ret = rt_task_wait_period(&newMissedReleasePoints);
+			if (ret != 0  &&  ret != -ETIMEDOUT) {  // ETIMEDOUT means that we missed a release point
+				syslog(LOG_ERR, "%s: rt_task_wait_period(): (%d) %s\n", __func__, -ret, strerror(-ret));
+				exit(2);
+			}
+			missedReleasePoints += newMissedReleasePoints;
 			start = rt_timer_read();
 
 			rtem->runExecutionCycle();
@@ -78,7 +113,7 @@ void rtemEntryPoint(void* cookie)
 			sumSq += duration * duration;
 			++loopCount;
 			if (duration > period_us) {
-				++missedCycleCount;
+				++overruns;
 			}
 		}
 	} catch (const ExecutionManagerException& e) {
@@ -98,12 +133,14 @@ void rtemEntryPoint(void* cookie)
 	double stdev = std::sqrt( ((double)sumSq/loopCount) - mean*mean );
 
     syslog(LOG_ERR, "RealTimeExecutionManager control-loop stats (microseconds):");
-    syslog(LOG_ERR, "  num missed cycles = %u (%u cycles total)", missedCycleCount, loopCount);
     syslog(LOG_ERR, "  target period = %u", period_us);
     syslog(LOG_ERR, "  min = %u", min);
     syslog(LOG_ERR, "  ave = %.3f", mean);
     syslog(LOG_ERR, "  max = %u", max);
     syslog(LOG_ERR, "  stdev = %.3f", stdev);
+    syslog(LOG_ERR, "  num total cycles = %u", loopCount);
+    syslog(LOG_ERR, "  num missed release points = %u", missedReleasePoints);
+    syslog(LOG_ERR, "  num overruns = %u", overruns);
 }
 
 
@@ -146,12 +183,28 @@ void RealTimeExecutionManager::start()
 	if ( !isRunning() ) {
 		stopRunning = false;
 		task = new RT_TASK;
-		rt_task_create(task, "RTEM", 0, priority, T_JOINABLE);
-		rt_task_start(task, &detail::rtemEntryPoint, reinterpret_cast<void*>(this));
+
+		std::string name = "RTEM: " + boost::lexical_cast<std::string>(this);
+		int ret = 0;
+		ret = rt_task_create(task, name.c_str(), 0, priority, T_JOINABLE);
+		if (ret != 0) {
+			syslog(LOG_ERR, "%s: rt_task_create(): (%d) %s\n", __func__, -ret, strerror(-ret));
+			throw std::runtime_error("systems::RealTimeExecutionManager::start(): Couldn't start the realtime task. See /var/log/syslog for details.");
+		}
+
+		ret = rt_task_start(task, &detail::rtemEntryPoint, reinterpret_cast<void*>(this));
+		if (ret != 0) {
+			syslog(LOG_ERR, "%s: rt_task_start(): (%d) %s\n", __func__, -ret, strerror(-ret));
+			throw std::runtime_error("systems::RealTimeExecutionManager::start(): Couldn't start the realtime task. See /var/log/syslog for details.");
+		}
 
 		// block until the thread starts reporting its new state
 		while ( !isRunning() ) {
-			rt_task_sleep(detail::secondsToRTIME(period / 10.0));
+			ret = rt_task_sleep(detail::secondsToRTIME(period / 10.0));
+			if (ret != 0) {
+				syslog(LOG_ERR, "%s: rt_task_sleep(): (%d) %s\n", __func__, -ret, strerror(-ret));
+				throw std::runtime_error("systems::RealTimeExecutionManager::start(): Couldn't start the realtime task. See /var/log/syslog for details.");
+			}
 		}
 	}
 	// TODO(dc): else, throw an exception?
@@ -160,9 +213,14 @@ void RealTimeExecutionManager::start()
 void RealTimeExecutionManager::stop()
 {
 	stopRunning = true;
-	rt_task_join(task);
 
-	rt_task_delete(task);
+	// According to Xenomai docs, rt_task_join() also performs the rt_task_delete() behaviors.
+	int ret = rt_task_join(task);
+	if (ret != 0) {
+		syslog(LOG_ERR, "%s: rt_task_join(): (%d) %s\n", __func__, -ret, strerror(-ret));
+		throw std::runtime_error("systems::RealTimeExecutionManager::stop(): Couldn't stop the realtime task. See /var/log/syslog for details.");
+	}
+
 	delete task;
 	task = NULL;
 }
@@ -175,7 +233,11 @@ void RealTimeExecutionManager::clearError()
 	errorStr = "";
 
 	stopRunning = true;
-	rt_task_delete(task);
+	int ret = rt_task_delete(task);
+	if (ret != 0) {
+		syslog(LOG_ERR, "%s: rt_task_delete(): (%d) %s\n", __func__, -ret, strerror(-ret));
+		throw std::runtime_error("systems::RealTimeExecutionManager::clearError(): Couldn't delete the realtime task. See /var/log/syslog for details.");
+	}
 	delete task;
 	task = NULL;
 }
