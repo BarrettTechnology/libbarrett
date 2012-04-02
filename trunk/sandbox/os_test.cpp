51f7c49a7bb65587c75a4588f0fb265014f4dbba
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2012-04-02 18:17:31 +0000

    Added btsleep(double) and btsleepRT(double) to os.h.

--- trunk/sandbox/os_test.cpp
+++ trunk/sandbox/os_test.cpp
@@ -9,17 +9,42 @@
 #include <string>
 #include <cassert>
 
+#include <boost/date_time.hpp>
+
 #include <barrett/exception.h>
 #include <barrett/os.h>
 
 
 using namespace barrett;
-
+namespace pt = boost::posix_time;
 
 int main() {
 	installExceptionHandler();
 
 
+	// btsleep()
+	pt::ptime startTime;
+
+	startTime = pt::microsec_clock::local_time();
+	btsleep(1.24);
+	std::cout << pt::microsec_clock::local_time() - startTime << "\n";
+
+	startTime = pt::microsec_clock::local_time();
+	btsleep(1e-4);
+	std::cout << pt::microsec_clock::local_time() - startTime << "\n";
+
+	startTime = pt::microsec_clock::local_time();
+	btsleep(3);
+	std::cout << pt::microsec_clock::local_time() - startTime << "\n";
+
+	// Fails if the thread is not realtime.
+//	startTime = pt::microsec_clock::local_time();
+//	btsleepRT(1e-3);
+//	std::cout << pt::microsec_clock::local_time() - startTime << "\n";
+
+
+
+	// logMessage()
 	logMessage("literal string\n");
 
 	const char cStr[] = "char array";
