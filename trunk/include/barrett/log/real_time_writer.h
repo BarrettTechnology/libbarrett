68b3ebe93ff01414f499d78c46a4476388856c14
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2012-05-04 18:56:45 +0000

    Changed the log::RealTimeWriter ctor to take its time parameter in seconds instead of microseconds.

--- trunk/include/barrett/log/real_time_writer.h
+++ trunk/include/barrett/log/real_time_writer.h
@@ -1,4 +1,27 @@
 /*
+	Copyright 2009, 2010, 2011, 2012 Barrett Technology <support@barrett.com>
+
+	This file is part of libbarrett.
+
+	This version of libbarrett is free software: you can redistribute it
+	and/or modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This version of libbarrett is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this version of libbarrett.  If not, see
+	<http://www.gnu.org/licenses/>.
+
+	Further, non-binding information about licensing is available at:
+	<http://wiki.barrett.com/libbarrett/wiki/LicenseNotes>
+*/
+
+/*
  * real_time_writer.h
  *
  *  Created on: Dec 30, 2009
@@ -27,7 +50,7 @@ public:
 	typedef typename Writer<T, Traits>::parameter_type parameter_type;
 
 	RealTimeWriter(const char* fileName, double recordPeriod_s);
-	RealTimeWriter(const char* fileName, size_t approxPeriod_us, size_t recordsInSingleBuffer);
+	RealTimeWriter(const char* fileName, double approxPeriod_s, size_t recordsInSingleBuffer);
 	~RealTimeWriter();
 
 	void putRecord(parameter_type data);
@@ -37,7 +60,8 @@ protected:
 	void init(size_t recordsInSingleBuffer);
 	void writeToDiskEntryPoint();
 
-	size_t period, singleBufferSize;
+	double period;
+	size_t singleBufferSize;
 	char* inBuff;
 	char* outBuff;
 	char* endInBuff;
