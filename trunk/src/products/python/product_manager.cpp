65cff11f0417748536d166939bc72fbe2bdb51af
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2012-06-13 20:32:11 +0000

    Made python wrapper for ProductManager. (Untested.)

--- trunk/src/products/python/product_manager.cpp
+++ trunk/src/products/python/product_manager.cpp
@@ -29,8 +29,11 @@
  */
 
 
+#include <string>
+
 #include <boost/python.hpp>
 
+#include <barrett/bus/abstract/communications_bus.h>
 #include <barrett/products/product_manager.h>
 #include <barrett/systems/wam.h>
 #include <barrett/systems/real_time_execution_manager.h>
@@ -42,23 +45,80 @@ using namespace barrett;
 using namespace boost::python;
 
 
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getExecutionManager_overloads, getExecutionManager, 0, 2)
+// Convert char array to a string, otherwise boost::python only recognizes the
+// first character.
+const std::string DEFAULT_CONFIG_FILE(ProductManager::DEFAULT_CONFIG_FILE);
+
+const size_t MAX_WAM_DOF = ProductManager::MAX_WAM_DOF;
+const double DEFAULT_LOOP_PERIOD = ProductManager::DEFAULT_LOOP_PERIOD;
+const int SAFETY_MODULE_ID = ProductManager::SAFETY_MODULE_ID;
+const int FIRST_WAM_ID = ProductManager::FIRST_WAM_ID;
+const int FIRST_HAND_ID = ProductManager::FIRST_HAND_ID;
+const int FORCE_TORQUE_SENSOR_ID = ProductManager::FORCE_TORQUE_SENSOR_ID;
+
+
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_waitForWam_overloads, waitForWam, 0, 1)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getWam4_overloads, getWam4, 0, 2)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getWam7_overloads, getWam7, 0, 2)
 
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(ProductManager_getExecutionManager_overloads, getExecutionManager, 0, 2)
+
 
 void pythonProductsProductManagerInterface() {
 	class_<ProductManager, boost::noncopyable>("ProductManager")
-		.def("getExecutionManager", &ProductManager::getExecutionManager,
-				ProductManager_getExecutionManager_overloads()[return_internal_reference<>()])
-		.def("waitForWam", &ProductManager::waitForWam, ProductManager_waitForWam_overloads())
+		.def(init<const char*>())
+		.def(init<const char*, bus::CommunicationsBus*>()[with_custodian_and_ward<1,3>()])
+
+		.def_readonly("DEFAULT_CONFIG_FILE", DEFAULT_CONFIG_FILE)
+
+		.def("enumerate", &ProductManager::enumerate)
+		.def("cleanUpAfterEstop", &ProductManager::cleanUpAfterEstop)
 		.def("wakeAllPucks", &ProductManager::wakeAllPucks)
-		.def("foundWam7", &ProductManager::foundWam7)
-		.def("getWam7", &ProductManager::getWam7,
-				ProductManager_getWam7_overloads()[return_internal_reference<>()])
+
+		.def("foundSafetyModule", &ProductManager::foundSafetyModule)
+		.def("getSafetyModule", &ProductManager::getSafetyModule, return_internal_reference<>())
+
+//		.def("getWamPucks", &ProductManager::getWamPucks)  // TODO(dc): Need return_internal_reference<>()?
+		.def("foundWam", &ProductManager::foundWam)
 		.def("foundWam4", &ProductManager::foundWam4)
+		.def("foundWam7", &ProductManager::foundWam7)
+		.def("foundWam7Wrist", &ProductManager::foundWam7Wrist)
+		.def("foundWam7Gimbals", &ProductManager::foundWam7Gimbals)
+
+		.def("waitForWam", &ProductManager::waitForWam, ProductManager_waitForWam_overloads())
+		.def("getWamDefaultConfigPath", &ProductManager::getWamDefaultConfigPath)
 		.def("getWam4", &ProductManager::getWam4,
 				ProductManager_getWam4_overloads()[return_internal_reference<>()])
+		.def("getWam7", &ProductManager::getWam7,
+				ProductManager_getWam7_overloads()[return_internal_reference<>()])
+
+		.def("getExecutionManager", &ProductManager::getExecutionManager,
+				ProductManager_getExecutionManager_overloads()[return_internal_reference<>()])
+		.def("startExecutionManager", &ProductManager::startExecutionManager)
+
+		.def("foundForceTorqueSensor", &ProductManager::foundForceTorqueSensor)
+		.def("getForceTorqueSensor", &ProductManager::getForceTorqueSensor, return_internal_reference<>())
+
+//		.def("getHandPucks", &ProductManager::getHandPucks)
+		.def("foundHand", &ProductManager::foundHand)
+		.def("getHand", &ProductManager::getHand, return_internal_reference<>())
+
+		.def("foundGimbalsHandController", &ProductManager::foundGimbalsHandController)
+		.def("getGimbalsHandController", &ProductManager::getGimbalsHandController, return_internal_reference<>())
+
+//		.def("getPucks", &ProductManager::getPucks, return_internal_reference<>())
+		.def("getPuck", &ProductManager::getPuck, return_internal_reference<>())
+//		.def("deletePuck", &ProductManager::deletePuck)
+
+		.def("getConfig", &ProductManager::getConfig, return_internal_reference<>())
+		.def("getBus", &ProductManager::getBus, return_internal_reference<>())
+		.def("getMutex", &ProductManager::getMutex, return_internal_reference<>())
+
+		.def_readonly("MAX_WAM_DOF", MAX_WAM_DOF)
+		.def_readonly("DEFAULT_LOOP_PERIOD", DEFAULT_LOOP_PERIOD)
+		.def_readonly("SAFETY_MODULE_ID", SAFETY_MODULE_ID)
+		.def_readonly("FIRST_WAM_ID", FIRST_WAM_ID)
+		.def_readonly("FIRST_HAND_ID", FIRST_HAND_ID)
+		.def_readonly("FORCE_TORQUE_SENSOR_ID", FORCE_TORQUE_SENSOR_ID)
 	;
 }
