22e7e8858eaf595f44c564836c6ae9e6609e177c
dc <dc@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2013-06-20 19:55:03 +0000

    Moved Xenomai-specific support code into an opaque implementation class.

--- trunk/src/thread/real_time_mutex.cpp
+++ trunk/src/thread/real_time_mutex.cpp
@@ -31,90 +31,53 @@
 #include <iostream>
 #include <stdexcept>
 
-#include <signal.h>
-
-#include <native/task.h>
-#include <native/mutex.h>
-
 #include <barrett/os.h>
-#include <barrett/detail/stacktrace.h>
 #include <barrett/thread/real_time_mutex.h>
+
+
+#ifdef BARRETT_XENOMAI
+#include "real_time_mutex_impl-xenomai.cpp"
+#else
+#include "real_time_mutex_impl-xenomai.cpp"
+#endif
 
 
 namespace barrett {
 namespace thread {
 
-namespace detail {
-
-struct mutex_impl : public rt_mutex_placeholder {};
-
-extern "C" {
-
-void warnOnSwitchToSecondaryMode(int)
-{
-	logMessage("WARNING: Switched out of RealTime. Stack-trace:",true);
-	barrett::detail::syslog_stacktrace();
-}
-
-
-}
-}
-
 
 RealTimeMutex::RealTimeMutex() :
-	mutex(NULL), lockCount(0), leaveWarnSwitchOn(false)
+	mutex(NULL), lockCount(0)
 {
 	mutex = new detail::mutex_impl;
-	int ret = rt_mutex_create(mutex, NULL);
-	if (ret != 0) {
-		(logMessage("thread::RealTimeMutex::%s:  Could not create RT_MUTEX: (%d) %s") %__func__ %-ret %strerror(-ret)).raise<std::logic_error>();
-	}
-
-	// Handler for warnings about falling out of real time mode
-	signal(SIGXCPU, &detail::warnOnSwitchToSecondaryMode);
 }
 
 RealTimeMutex::~RealTimeMutex()
 {
-	int ret = rt_mutex_delete(mutex);
 	delete mutex;
 	mutex = NULL;
-
-	if (ret != 0) {
-		(logMessage("thread::RealTimeMutex::%s:  Could not delete RT_MUTEX: (%d) %s") %__func__ %-ret %strerror(-ret)).raise<std::logic_error>();
-	}
 }
 
 void RealTimeMutex::lock()
 {
-    int ret = acquireWrapper(true);
-    if (ret != 0) {
-	logMessage("RealTimeMutex::lock(): %s returned %d") %__func__ % ret;
-	throw boost::thread_resource_error(ret);
-    }
+	mutex->lock();
+	++lockCount;
 }
 
 bool RealTimeMutex::try_lock()
 {
-	return acquireWrapper(false) == 0;
+	if (mutex->try_lock()) {
+		++lockCount;
+		return true;
+	} else {
+		return false;
+	}
 }
 
 void RealTimeMutex::unlock()
 {
 	--lockCount;
-	bool changeMode = lockCount == 0 && !leaveWarnSwitchOn;
-
-	int ret = rt_mutex_release(mutex);
-	if (ret != 0) {
-		(logMessage("thread::RealTimeMutex::%s:  Could not release RT_MUTEX: (%d) %s") %__func__ %-ret %strerror(-ret)).raise<std::logic_error>();
-	}
-
-	if (changeMode) {
-		ret = rt_task_set_mode(T_WARNSW, 0, NULL);
-		if (ret != 0) {
-			throw std::runtime_error("thread::RealTimeMutex::unlock(): Could not clear T_WARNSW mode.");
-		}
-	}
+	mutex->unlock();
 }
 
 int RealTimeMutex::fullUnlock()
@@ -138,43 +101,6 @@ void RealTimeMutex::relock(int lc)
 	while (lockCount != lc) {
 		lock();
 	}
-}
-
-
-int RealTimeMutex::acquireWrapper(bool blocking)
-{
-	const RTIME timeout = blocking ? TM_INFINITE : TM_NONBLOCK;
-	int ret;
-
-	ret = rt_mutex_acquire(mutex, timeout);
-	if (ret == -EPERM) {
-		// become real-time, then try again
-
-		// Allocate a new RT_TASK struct, and then forget the pointer. This
-		// leak allows us to avoid ownership issues for the RT_TASK, which
-		// shouldn't necessarily be deleted when the mutex is released, or when
-		// the mutex is deleted, etc.. It is a small overhead that happens (at
-		// most) once per thread. If needed, we can always get the pointer back
-		// by calling rt_task_self().
-		rt_task_shadow(new RT_TASK, NULL, 10, 0);
-
-		ret = rt_mutex_acquire(mutex, timeout);
-	}
-	if (ret != 0) {
-		return ret;
-	}
-
-	if (lockCount == 0) {
-		int oldMode;
-		ret = rt_task_set_mode(0, T_WARNSW, &oldMode);
-		if (ret != 0) {
-			throw std::runtime_error("thread::RealTimeMutex::acquireWrapper(): Could not set T_WARNSW mode.");
-		}
-		leaveWarnSwitchOn = oldMode & T_WARNSW;
-	}
-	++lockCount;
-
-	return ret;
 }
 
 
