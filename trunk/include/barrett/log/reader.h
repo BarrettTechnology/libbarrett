932819d630c85c57438434d0d9a5c35ad28e0447
jh <jh@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2014-05-30 17:00:17 +0000

    Doxygen Updates to Copywrite and name/date/author presentations

--- trunk/include/barrett/log/reader.h
+++ trunk/include/barrett/log/reader.h
@@ -1,8 +1,33 @@
-/*
- * reader.h
+/**
+ *	Copyright 2009-2014 Barrett Technology <support@barrett.com>
  *
- *  Created on: Dec 29, 2009
- *      Author: dc
+ *	This file is part of libbarrett.
+ *
+ *	This version of libbarrett is free software: you can redistribute it
+ *	and/or modify it under the terms of the GNU General Public License as
+ *	published by the Free Software Foundation, either version 3 of the
+ *	License, or (at your option) any later version.
+ *
+ *	This version of libbarrett is distributed in the hope that it will be
+ *	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this version of libbarrett.  If not, see
+ *	<http://www.gnu.org/licenses/>.
+ *
+ *
+ *	Barrett Technology Inc.
+ *	73 Chapel Street
+ *	Newton, MA 02458
+ */
+
+/**
+ * @file reader.h
+ * @date 12/29/2009
+ * @author Dan Cody
+ *  
  */
 
 #ifndef BARRETT_LOG_READER_H_
@@ -22,16 +47,31 @@ template<typename T, typename Traits = Traits<T> >
 class Reader {
 public:
 	typedef typename Traits::parameter_type parameter_type;
-
+	/** Constructor and Destructors for Reader.
+	 *
+	 */
 	Reader(const char* fileName);
 	~Reader();
 
+/** numRecords Method returns the number of lines in the file being processed.
+ *
+ */
 	size_t numRecords() const;
-
+/** getRecord Method returns the line of the file currently being processed.
+ *
+ */
 	T getRecord();
+/** exportCSV method writes binary data to comma separated text file.
+ *
+ */
 	void exportCSV(const char* outputFileName);
+/** exportCSV method writes binary data to comma separated text file.
+ *
+ */
 	void exportCSV(std::ostream& os);
-
+/** close Method destroys file being written to. 
+ *
+ */
 	void close();
 
 protected:
