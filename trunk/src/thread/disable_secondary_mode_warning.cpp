245668870b73272fe598fdfab6c087e8cb4ce18e
jh <jh@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2014-06-19 01:19:03 +0000

    updated Copyright

--- trunk/src/thread/disable_secondary_mode_warning.cpp
+++ trunk/src/thread/disable_secondary_mode_warning.cpp
@@ -1,8 +1,33 @@
-/*
- * disable_secondary_mode_warning.cpp
+/**
+ *	Copyright 2009-2014 Barrett Technology <support@barrett.com>
  *
- *  Created on: Dec 14, 2010
- *      Author: dc
+ *	This file is part of libbarrett.
+ *
+ *	This version of libbarrett is free software: you can redistribute it
+ *	and/or modify it under the terms of the GNU General Public License as
+ *	published by the Free Software Foundation, either version 3 of the
+ *	License, or (at your option) any later version.
+ *
+ *	This version of libbarrett is distributed in the hope that it will be
+ *	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License along
+ *	with this version of libbarrett.  If not, see
+ *	<http://www.gnu.org/licenses/>.
+ *
+ *
+ *	Barrett Technology Inc.
+ *	73 Chapel Street
+ *	Newton, MA 02458
+ *
+ */
+/*
+ * @file disable_secondary_mode_warning.cpp
+ * @date 12/14/2010
+ * @author Dan Cody
+ * 
  */
 
 #ifdef BARRETT_XENOMAI
